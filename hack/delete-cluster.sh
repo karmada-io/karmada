228ef19444e75678642881f9b77d540fe6a28e75
RainbowMango <renhongcai@huawei.com>
2020-11-26 16:50:41 +0800

    Add scripts for create/delete kind cluster and deploy/undeploy control-plane.

---
+++ hack/delete-cluster.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+function usage() {
+  echo "This script delete a kube cluster by kind."
+  echo "Usage: hack/delete-cluster.sh <CLUSTER_NAME> <KUBECONFIG>"
+  echo "Example: hack/delete-cluster.sh host /root/.kube/host.config"
+}
+
+if [[ $# -ne 2 ]]; then
+  usage
+  exit 1
+fi
+
+CLUSTER_NAME=$1
+if [[ -z "${CLUSTER_NAME}" ]]; then
+  usage
+  exit 1
+fi
+KUBECONFIG=$2
+if [[ -z "${KUBECONFIG}" ]]; then
+  usage
+  exit 1
+fi
+
+# The context name has been changed when creating clusters by 'create-cluster.sh'.
+# This will result in the context can't be removed by kind when deleting a cluster.
+# So, we need to change context name back and let kind take care about it.
+kubectl config rename-context "${CLUSTER_NAME}" "kind-${CLUSTER_NAME}" --kubeconfig="${KUBECONFIG}"
+
+kind delete cluster --name "${CLUSTER_NAME}" --kubeconfig="${KUBECONFIG}"
