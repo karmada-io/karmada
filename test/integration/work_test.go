e49f9d6ea7b97652d1a0ade7b32c6fc12ea108b1
RainbowMango <renhongcai@huawei.com>
2020-12-01 10:39:10 +0800

    Add integration and e2e framework

---
+++ test/integration/work_test.go
@@ -0,0 +1,9 @@
+package integration
+
+import "github.com/onsi/ginkgo"
+
+var _ = ginkgo.Describe("Work", func() {
+	ginkgo.BeforeEach(func() {
+		// prepare work, such as initialize objects used by this test.
+	})
+})
