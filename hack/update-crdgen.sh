6b44a3a4561a5de4cff6733da2c9d7b3355141d5
RainbowMango <renhongcai@huawei.com>
2020-12-04 10:57:17 +0800

    pin controller-gen with version v0.4.1

--- hack/update-crdgen.sh
+++ hack/update-crdgen.sh
@@ -4,4 +4,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+CONTROLLER_GEN_VERSION="v0.4.1"
+
+# Install tools we need.
+TEMP_PATH=$(mktemp -d)
+pushd "${TEMP_PATH}" >/dev/null
+  GO111MODULE=on go get sigs.k8s.io/controller-tools/cmd/controller-gen@"${CONTROLLER_GEN_VERSION}"
+popd >/dev/null
+rm -rf "${TEMP_PATH}"
+
 controller-gen crd paths=./pkg/apis/... output:crd:dir=./artifacts/deploy
