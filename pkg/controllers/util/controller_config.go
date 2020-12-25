1c760c6b1f37acc6f80bb2d7aea3fc9355e31026
chenxianpao <chenxianpao@huawei.com>
2020-12-25 15:47:49 +0800

    remove unnecessary file

--- pkg/controllers/util/controller_config.go
+++
@@ -1,9 +0,0 @@
-package util
-
-import "k8s.io/client-go/rest"
-
-// ControllerConfig defines the common configuration shared by most of controllers.
-type ControllerConfig struct {
-	// HeadClusterConfig holds the configuration of head cluster.
-	HeadClusterConfig *rest.Config
-}
