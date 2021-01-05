60507128b2239d2f47bd0a825a04902a474388ca
lihanbo <lihanbo2@huawei.com>
2021-01-05 16:15:54 +0800

    add object watcher to manage operations for object dispatched to member clusters
    Signed-off-by: lihanbo <lihanbo2@huawei.com>

--- pkg/util/label.go
+++ pkg/util/label.go
@@ -1,5 +1,9 @@
 package util
 
+import (
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
 // GetLabelValue retrieves the value via 'labelKey' if exist, otherwise returns an empty string.
 func GetLabelValue(labels map[string]string, labelKey string) string {
 	if labels == nil {
@@ -7,4 +11,14 @@ func GetLabelValue(labels map[string]string, labelKey string) string {
 	}
 
 	return labels[labelKey]
+}
+
+// MergeLabel adds label for the given object.
+func MergeLabel(obj *unstructured.Unstructured, labelKey string, labelValue string) {
+	workloadLabel := obj.GetLabels()
+	if workloadLabel == nil {
+		workloadLabel = make(map[string]string, 1)
+	}
+	workloadLabel[labelKey] = labelValue
+	obj.SetLabels(workloadLabel)
 }
