7f232cde7505f81f6eebf09951e62e934a8d8d72
Hongcai Ren <renhongcai@huawei.com>
2021-01-22 19:09:10 +0800

    enable register-gen to generate register code (#138)
    Signed-off-by: RainbowMango <renhongcai@huawei.com>

--- pkg/apis/membercluster/register.go
+++
@@ -1,6 +0,0 @@
-package membercluster
-
-// GroupName is the group name used in this package
-const (
-	GroupName = "membercluster.karmada.io"
-)
--- pkg/apis/membercluster/v1alpha1/doc.go
+++ pkg/apis/membercluster/v1alpha1/doc.go
@@ -1,5 +1,4 @@
-// +k8s:deepcopy-gen=package
-// +groupName=membercluster.karmada.io
-
 // Package v1alpha1 is the v1alpha1 version of the API.
+// +k8s:deepcopy-gen=package,register
+// +groupName=membercluster.karmada.io
 package v1alpha1
