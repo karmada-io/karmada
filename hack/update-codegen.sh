7b2f2dd62d25ac010b070e73e1a42090336f4521
RainbowMango <renhongcai@huawei.com>
2021-01-22 15:35:26 +0800

    enable register-gen to generate register code
    Signed-off-by: RainbowMango <renhongcai@huawei.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -12,3 +12,14 @@ bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
   "propagationstrategy:v1alpha1 membercluster:v1alpha1" \
   --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate/boilerplate.go.txt
+
+echo "Generating with register-gen"
+GO111MODULE=on go install k8s.io/code-generator/cmd/register-gen
+register-gen \
+  --go-header-file hack/boilerplate/boilerplate.go.txt \
+  --input-dirs=github.com/karmada-io/karmada/pkg/apis/membercluster/v1alpha1 \
+  --output-package=github.com/karmada-io/karmada/pkg/apis/membercluster/v1alpha1
+register-gen \
+  --go-header-file hack/boilerplate/boilerplate.go.txt \
+  --input-dirs=github.com/karmada-io/karmada/pkg/apis/propagationstrategy/v1alpha1 \
+  --output-package=github.com/karmada-io/karmada/pkg/apis/propagationstrategy/v1alpha1
