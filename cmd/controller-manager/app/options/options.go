d13875b2c3cdffb2a6906607d70ba9c16120ebb4
Hongcai Ren <renhongcai@huawei.com>
2021-01-30 15:15:48 +0800

    enable karmada-controller-manager health check endpoint /healthz (#154)
    Signed-off-by: RainbowMango <renhongcai@huawei.com>

--- cmd/controller-manager/app/options/options.go
+++ cmd/controller-manager/app/options/options.go
@@ -3,6 +3,7 @@ package options
 import (
 	"time"
 
+	"github.com/spf13/pflag"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	componentbaseconfig "k8s.io/component-base/config"
@@ -15,10 +16,20 @@ var (
 	defaultElectionRetryPeriod   = metav1.Duration{Duration: 2 * time.Second}
 )
 
+const (
+	defaultBindAddress = "0.0.0.0"
+	defaultPort        = 10357
+)
+
 // Options contains everything necessary to create and run controller-manager.
 type Options struct {
 	HostNamespace  string
 	LeaderElection componentbaseconfig.LeaderElectionConfiguration
+	// BindAddress is the IP address on which to listen for the --secure-port port.
+	BindAddress string
+	// SecurePort is the port that the the server serves at.
+	// Note: We hope support https in the future once controller-runtime provides the functionality.
+	SecurePort int
 }
 
 // NewOptions builds an empty options.
@@ -52,4 +63,12 @@ func (o *Options) Complete() {
 		o.LeaderElection.RetryPeriod = defaultElectionRetryPeriod
 		klog.Infof("Set default value: Options.LeaderElection.RetryPeriod = %s", defaultElectionRetryPeriod.Duration.String())
 	}
+}
+
+// AddFlags adds flags to the specified FlagSet.
+func (o *Options) AddFlags(flags *pflag.FlagSet) {
+	flags.StringVar(&o.BindAddress, "bind-address", defaultBindAddress,
+		"The IP address on which to listen for the --secure-port port.")
+	flags.IntVar(&o.SecurePort, "secure-port", defaultPort,
+		"The secure port on which to serve HTTPS.")
 }
