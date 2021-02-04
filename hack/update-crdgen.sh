11a65f22178da5b648683af72047eeaf7ec9d622
Hongcai Ren <renhongcai@huawei.com>
2021-02-04 15:33:48 +0800

    make update codegen more simpler (#161)
    Signed-off-by: RainbowMango <renhongcai@huawei.com>

--- hack/update-crdgen.sh
+++ hack/update-crdgen.sh
@@ -9,6 +9,6 @@ CONTROLLER_GEN_VER="v0.4.1"
 
 source hack/util.sh
 
-util::install_tools ${CONTROLLER_GEN_PKG} ${CONTROLLER_GEN_VER}
-
+echo "Generating with controller-gen"
+util::install_tools ${CONTROLLER_GEN_PKG} ${CONTROLLER_GEN_VER} >/dev/null 2>&1
 controller-gen crd paths=./pkg/apis/... output:crd:dir=./artifacts/deploy
