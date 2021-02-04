8009359682186906aaedcfd9a0a9566335d8d1a4
RainbowMango <renhongcai@huawei.com>
2021-02-04 11:16:43 +0800

    make update codegen more simpler
    Signed-off-by: RainbowMango <renhongcai@huawei.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -4,22 +4,57 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+# For all commands, the working directory is the parent directory(repo root).
+REPO_ROOT=$(git rev-parse --show-toplevel)
+cd "${REPO_ROOT}"
 
-bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
-  github.com/karmada-io/karmada/pkg/generated github.com/karmada-io/karmada/pkg/apis \
-  "policy:v1alpha1 cluster:v1alpha1" \
-  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
-  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate/boilerplate.go.txt
+echo "Generating with deepcopy-gen"
+GO111MODULE=on go install k8s.io/code-generator/cmd/deepcopy-gen
+deepcopy-gen \
+  --go-header-file hack/boilerplate/boilerplate.go.txt \
+  --input-dirs=github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1 \
+  --output-package=github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1 \
+  --output-file-base=zz_generated.deepcopy
+deepcopy-gen \
+  --go-header-file hack/boilerplate/boilerplate.go.txt \
+  --input-dirs=github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1 \
+  --output-package=github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1 \
+  --output-file-base=zz_generated.deepcopy
 
 echo "Generating with register-gen"
 GO111MODULE=on go install k8s.io/code-generator/cmd/register-gen
 register-gen \
   --go-header-file hack/boilerplate/boilerplate.go.txt \
   --input-dirs=github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1 \
-  --output-package=github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1
+  --output-package=github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1 \
+  --output-file-base=zz_generated.register
 register-gen \
   --go-header-file hack/boilerplate/boilerplate.go.txt \
   --input-dirs=github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1 \
-  --output-package=github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1
+  --output-package=github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1 \
+  --output-file-base=zz_generated.register
+
+echo "Generating with client-gen"
+GO111MODULE=on go install k8s.io/code-generator/cmd/client-gen
+client-gen \
+  --go-header-file hack/boilerplate/boilerplate.go.txt \
+  --input-base="" \
+  --input=github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1,github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1 \
+  --output-package=github.com/karmada-io/karmada/pkg/generated/clientset \
+  --clientset-name=versioned
+
+echo "Generating with lister-gen"
+GO111MODULE=on go install k8s.io/code-generator/cmd/lister-gen
+lister-gen \
+  --go-header-file hack/boilerplate/boilerplate.go.txt \
+  --input-dirs=github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1,github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1 \
+  --output-package=github.com/karmada-io/karmada/pkg/generated/listers
+
+echo "Generating with informer-gen"
+GO111MODULE=on go install k8s.io/code-generator/cmd/informer-gen
+informer-gen \
+  --go-header-file hack/boilerplate/boilerplate.go.txt \
+  --input-dirs=github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1,github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1 \
+  --versioned-clientset-package=github.com/karmada-io/karmada/pkg/generated/clientset/versioned \
+  --listers-package=github.com/karmada-io/karmada/pkg/generated/listers \
+  --output-package=github.com/karmada-io/karmada/pkg/generated/informers
