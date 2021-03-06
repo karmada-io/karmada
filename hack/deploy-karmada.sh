e804a0da58965e09ab63c96ad7e29727d5bbc099
RainbowMango <renhongcai@huawei.com>
2021-03-06 11:37:27 +0800

    Update deploy scripts after API group changed
    Signed-off-by: RainbowMango <renhongcai@huawei.com>

--- hack/deploy-karmada.sh
+++ hack/deploy-karmada.sh
@@ -69,8 +69,8 @@ function installCRDs() {
     kubectl apply -f "${REPO_ROOT}/artifacts/deploy/cluster.karmada.io_clusters.yaml"
     kubectl apply -f "${REPO_ROOT}/artifacts/deploy/policy.karmada.io_propagationpolicies.yaml"
     kubectl apply -f "${REPO_ROOT}/artifacts/deploy/policy.karmada.io_resourcebindings.yaml"
-    kubectl apply -f "${REPO_ROOT}/artifacts/deploy/policy.karmada.io_works.yaml"
     kubectl apply -f "${REPO_ROOT}/artifacts/deploy/policy.karmada.io_overridepolicies.yaml"
+    kubectl apply -f "${REPO_ROOT}/artifacts/deploy/work.karmada.io_works.yaml"
 }
 
 #generate cert
