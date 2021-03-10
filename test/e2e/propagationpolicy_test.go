2a111b4b22df6120fc16255574500dd775699f2f
Hongcai Ren <renhongcai@huawei.com>
2021-03-10 18:46:23 +0800

    setup basic propagation testing (#207)
    Signed-off-by: RainbowMango <renhongcai@huawei.com>

--- test/e2e/propagationpolicy_test.go
+++ test/e2e/propagationpolicy_test.go
@@ -2,54 +2,62 @@ package e2e
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/rand"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/klog/v2"
+	"k8s.io/utils/pointer"
 
 	"github.com/karmada-io/karmada/test/helper"
 )
 
-var _ = ginkgo.Describe("[propagation policy] propagation policy functionality testing", func() {
-	deploymentName := rand.String(6)
-	deployment := helper.NewDeployment(testNamespace, deploymentName)
-	var err error
+// BasicPropagation focus on basic propagation functionality testing.
+var _ = ginkgo.Describe("[BasicPropagation] basic propagation testing", func() {
+	ginkgo.Context("Deployment propagation testing", func() {
+		policyNamespace := testNamespace
+		policyName := "deploy-" + rand.String(3)
+		deploymentNamespace := testNamespace
+		deploymentName := policyName
 
-	ginkgo.BeforeEach(func() {
-		ginkgo.By(fmt.Sprintf("creating deployment(%s/%s)", deployment.Namespace, deployment.Name), func() {
-			_, err = kubeClient.AppsV1().Deployments(testNamespace).Create(context.TODO(), deployment, metav1.CreateOptions{})
-			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-		})
-	})
+		deployment := helper.NewDeployment(deploymentNamespace, deploymentName)
+		policy := helper.NewPolicyWithSingleDeployment(policyNamespace, policyName, deployment, clusterNames)
 
-	ginkgo.AfterEach(func() {
-		err = kubeClient.CoreV1().Namespaces().Delete(context.TODO(), testNamespace, metav1.DeleteOptions{})
-		gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-	})
-
-	// propagate single resource to two explicit clusters.
-	ginkgo.Context("single resource propagation testing", func() {
-		ginkgo.It("propagate deployment", func() {
-			policyName := rand.String(6)
-			policyNamespace := testNamespace // keep policy in the same namespace with the resource
-			policy := helper.NewPolicyWithSingleDeployment(policyNamespace, policyName, deployment, clusterNames)
-
+		// prepare deployment and policy before each It block, so that each block should not depend on each other.
+		ginkgo.BeforeEach(func() {
 			ginkgo.By(fmt.Sprintf("creating policy: %s/%s", policyNamespace, policyName), func() {
-				_, err = karmadaClient.PolicyV1alpha1().PropagationPolicies(policyNamespace).Create(context.TODO(), policy, metav1.CreateOptions{})
+				_, err := karmadaClient.PolicyV1alpha1().PropagationPolicies(policyNamespace).Create(context.TODO(), policy, metav1.CreateOptions{})
 				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 			})
-			ginkgo.By("check if resource appear in member clusters", func() {
+		})
+
+		ginkgo.AfterEach(func() {
+			ginkgo.By(fmt.Sprintf("removing policy(%s/%s)", policyNamespace, policyName), func() {
+				err := karmadaClient.PolicyV1alpha1().PropagationPolicies(policyNamespace).Delete(context.TODO(), policyName, metav1.DeleteOptions{})
+				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			})
+		})
+
+		ginkgo.It("deployment propagation testing", func() {
+			ginkgo.By(fmt.Sprintf("creating deployment(%s/%s)", deploymentNamespace, deploymentName), func() {
+				_, err := kubeClient.AppsV1().Deployments(testNamespace).Create(context.TODO(), deployment, metav1.CreateOptions{})
+				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			})
+
+			ginkgo.By("check if deployment present on member clusters", func() {
 				for _, cluster := range clusters {
 					clusterClient := getClusterClient(cluster.Name)
 					gomega.Expect(clusterClient).ShouldNot(gomega.BeNil())
 
-					// Temporarily increase the timeout time(remove when refactor)
-					err = wait.Poll(pollInterval, 2*pollTimeout, func() (done bool, err error) {
-						_, err = clusterClient.AppsV1().Deployments(deployment.Namespace).Get(context.TODO(), deployment.Name, metav1.GetOptions{})
+					klog.Infof("Waiting for deployment(%s/%s) present on cluster(%s)", deploymentNamespace, deploymentName, cluster.Name)
+					err := wait.Poll(pollInterval, pollTimeout, func() (done bool, err error) {
+						_, err = clusterClient.AppsV1().Deployments(deploymentNamespace).Get(context.TODO(), deploymentName, metav1.GetOptions{})
 						if err != nil {
 							if errors.IsNotFound(err) {
 								return false, nil
@@ -62,33 +70,66 @@ var _ = ginkgo.Describe("[propagation policy] propagation policy functionality t
 				}
 			})
 
-			ginkgo.By(fmt.Sprintf("deleting policy: %s/%s", policyNamespace, policyName), func() {
-				err = karmadaClient.PolicyV1alpha1().PropagationPolicies(policyNamespace).Delete(context.TODO(), policyName, metav1.DeleteOptions{})
+			ginkgo.By("updating deployment", func() {
+				patch := map[string]interface{}{
+					"spec": map[string]interface{}{
+						"replicas": pointer.Int32Ptr(6),
+					},
+				}
+				bytes, err := json.Marshal(patch)
+				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+				_, err = kubeClient.AppsV1().Deployments(deploymentNamespace).Patch(context.TODO(), deploymentName, types.StrategicMergePatchType, bytes, metav1.PatchOptions{})
 				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 			})
-			/* Needs refactor
-			ginkgo.By("check if resource disappear from member clusters", func() {
+
+			ginkgo.By("check if update has been synced to member clusters", func() {
 				for _, cluster := range clusters {
 					clusterClient := getClusterClient(cluster.Name)
 					gomega.Expect(clusterClient).ShouldNot(gomega.BeNil())
 
-					err = wait.Poll(pollInterval, pollTimeout, func() (done bool, err error) {
-						_, err = clusterClient.AppsV1().Deployments(deployment.Namespace).Get(context.TODO(), deployment.Name, metav1.GetOptions{})
+					klog.Infof("Waiting for deployment(%s/%s) synced on cluster(%s)", deploymentNamespace, deploymentName, cluster.Name)
+					err := wait.Poll(pollInterval, pollTimeout, func() (done bool, err error) {
+						dep, err := clusterClient.AppsV1().Deployments(deploymentNamespace).Get(context.TODO(), deploymentName, metav1.GetOptions{})
 						if err != nil {
-							if errors.IsNotFound(err) {
-								return true, nil
-							}
+							return false, err
 						}
+
+						if *dep.Spec.Replicas == 6 {
+							return true, nil
+						}
+
 						return false, nil
 					})
 					gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 				}
 			})
-			*/
-		})
-	})
 
-	// propagate two resource to two explicit clusters.
-	ginkgo.Context("multiple resource propagation testing", func() {
+			ginkgo.By(fmt.Sprintf("removing deployment(%s/%s)", deploymentNamespace, deploymentName), func() {
+				err := kubeClient.AppsV1().Deployments(testNamespace).Delete(context.TODO(), deploymentName, metav1.DeleteOptions{})
+				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			})
+
+			ginkgo.By("check if deployment has been deleted from member clusters", func() {
+				for _, cluster := range clusters {
+					clusterClient := getClusterClient(cluster.Name)
+					gomega.Expect(clusterClient).ShouldNot(gomega.BeNil())
+
+					klog.Infof("Waiting for deployment(%s/%s) disappear on cluster(%s)", deploymentNamespace, deploymentName, cluster.Name)
+					err := wait.Poll(pollInterval, pollTimeout, func() (done bool, err error) {
+						_, err = clusterClient.AppsV1().Deployments(deploymentNamespace).Get(context.TODO(), deploymentName, metav1.GetOptions{})
+						if err != nil {
+							if errors.IsNotFound(err) {
+								return true, nil
+							}
+							return false, err
+						}
+
+						return false, nil
+					})
+					gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+				}
+			})
+		})
 	})
 })
