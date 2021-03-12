b3dfc1d1f7ccb93cc27e6b28bf745ae20d05efa7
Zhen Chang <78244870+XiShanYongYe-Chang@users.noreply.github.com>
2021-03-12 09:50:22 +0800

    add basic service propagation testing (#212)
    Signed-off-by: changzhen <changzhen5@huawei.com>

--- test/e2e/propagationpolicy_test.go
+++ test/e2e/propagationpolicy_test.go
@@ -18,20 +18,27 @@ import (
 	"github.com/karmada-io/karmada/test/helper"
 )
 
+const (
+	deploymentNamePrefix = "deploy-"
+	serviceNamePrefix    = "service-"
+
+	updateDeploymentReplicas = 6
+	updateServicePort        = 81
+)
+
 // BasicPropagation focus on basic propagation functionality testing.
 var _ = ginkgo.Describe("[BasicPropagation] basic propagation testing", func() {
 	ginkgo.Context("Deployment propagation testing", func() {
 		policyNamespace := testNamespace
-		policyName := "deploy-" + rand.String(3)
+		policyName := deploymentNamePrefix + rand.String(RandomStrLength)
 		deploymentNamespace := testNamespace
 		deploymentName := policyName
 
 		deployment := helper.NewDeployment(deploymentNamespace, deploymentName)
 		policy := helper.NewPolicyWithSingleDeployment(policyNamespace, policyName, deployment, clusterNames)
 
-		// prepare deployment and policy before each It block, so that each block should not depend on each other.
 		ginkgo.BeforeEach(func() {
-			ginkgo.By(fmt.Sprintf("creating policy: %s/%s", policyNamespace, policyName), func() {
+			ginkgo.By(fmt.Sprintf("creating policy(%s/%s)", policyNamespace, policyName), func() {
 				_, err := karmadaClient.PolicyV1alpha1().PropagationPolicies(policyNamespace).Create(context.TODO(), policy, metav1.CreateOptions{})
 				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 			})
@@ -73,7 +80,7 @@ var _ = ginkgo.Describe("[BasicPropagation] basic propagation testing", func() {
 			ginkgo.By("updating deployment", func() {
 				patch := map[string]interface{}{
 					"spec": map[string]interface{}{
-						"replicas": pointer.Int32Ptr(6),
+						"replicas": pointer.Int32Ptr(updateDeploymentReplicas),
 					},
 				}
 				bytes, err := json.Marshal(patch)
@@ -95,7 +102,7 @@ var _ = ginkgo.Describe("[BasicPropagation] basic propagation testing", func() {
 							return false, err
 						}
 
-						if *dep.Spec.Replicas == 6 {
+						if *dep.Spec.Replicas == updateDeploymentReplicas {
 							return true, nil
 						}
 
@@ -118,6 +125,120 @@ var _ = ginkgo.Describe("[BasicPropagation] basic propagation testing", func() {
 					klog.Infof("Waiting for deployment(%s/%s) disappear on cluster(%s)", deploymentNamespace, deploymentName, cluster.Name)
 					err := wait.Poll(pollInterval, pollTimeout, func() (done bool, err error) {
 						_, err = clusterClient.AppsV1().Deployments(deploymentNamespace).Get(context.TODO(), deploymentName, metav1.GetOptions{})
+						if err != nil {
+							if errors.IsNotFound(err) {
+								return true, nil
+							}
+							return false, err
+						}
+
+						return false, nil
+					})
+					gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+				}
+			})
+		})
+	})
+
+	ginkgo.Context("Service propagation testing", func() {
+		policyNamespace := testNamespace
+		policyName := serviceNamePrefix + rand.String(RandomStrLength)
+		serviceNamespace := policyNamespace
+		serviceName := policyName
+
+		service := helper.NewService(serviceNamespace, serviceName)
+		policy := helper.NewPolicyWithSingleService(policyNamespace, policyName, service, clusterNames)
+
+		ginkgo.BeforeEach(func() {
+			ginkgo.By(fmt.Sprintf("creating policy(%s/%s)", policyNamespace, policyName), func() {
+				_, err := karmadaClient.PolicyV1alpha1().PropagationPolicies(policyNamespace).Create(context.TODO(), policy, metav1.CreateOptions{})
+				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			})
+		})
+
+		ginkgo.AfterEach(func() {
+			ginkgo.By(fmt.Sprintf("removing policy(%s/%s)", policyNamespace, policyName), func() {
+				err := karmadaClient.PolicyV1alpha1().PropagationPolicies(policyNamespace).Delete(context.TODO(), policyName, metav1.DeleteOptions{})
+				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			})
+		})
+
+		ginkgo.It("service propagation testing", func() {
+			ginkgo.By(fmt.Sprintf("creating service(%s/%s)", serviceNamespace, serviceName), func() {
+				_, err := kubeClient.CoreV1().Services(serviceNamespace).Create(context.TODO(), service, metav1.CreateOptions{})
+				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			})
+
+			ginkgo.By("check if service present on member clusters", func() {
+				for _, cluster := range clusters {
+					clusterClient := getClusterClient(cluster.Name)
+					gomega.Expect(clusterClient).ShouldNot(gomega.BeNil())
+
+					klog.Infof("Waiting for service(%s/%s) present on cluster(%s)", serviceNamespace, serviceName, cluster.Name)
+					err := wait.Poll(pollInterval, pollTimeout, func() (done bool, err error) {
+						_, err = clusterClient.CoreV1().Services(serviceNamespace).Get(context.TODO(), serviceName, metav1.GetOptions{})
+						if err != nil {
+							if errors.IsNotFound(err) {
+								return false, nil
+							}
+							return false, err
+						}
+						return true, nil
+					})
+					gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+				}
+			})
+
+			ginkgo.By("updating service", func() {
+				patch := []map[string]interface{}{
+					{
+						"op":    "replace",
+						"path":  "/spec/ports/0/port",
+						"value": updateServicePort,
+					},
+				}
+				bytes, err := json.Marshal(patch)
+				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+				_, err = kubeClient.CoreV1().Services(serviceNamespace).Patch(context.TODO(), serviceName, types.JSONPatchType, bytes, metav1.PatchOptions{})
+				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			})
+
+			ginkgo.By("check if update has been synced to member clusters", func() {
+				for _, cluster := range clusters {
+					clusterClient := getClusterClient(cluster.Name)
+					gomega.Expect(clusterClient).ShouldNot(gomega.BeNil())
+
+					klog.Infof("Waiting for service(%s/%s) synced on cluster(%s)", serviceNamespace, serviceName, cluster.Name)
+					err := wait.Poll(pollInterval, pollTimeout, func() (done bool, err error) {
+						service, err := clusterClient.CoreV1().Services(serviceNamespace).Get(context.TODO(), serviceName, metav1.GetOptions{})
+						if err != nil {
+							return false, err
+						}
+
+						if service.Spec.Ports[0].Port == updateServicePort {
+							return true, nil
+						}
+
+						return false, nil
+					})
+					gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+				}
+			})
+
+			ginkgo.By(fmt.Sprintf("removing service(%s/%s)", serviceNamespace, serviceName), func() {
+				err := kubeClient.CoreV1().Services(serviceNamespace).Delete(context.TODO(), serviceName, metav1.DeleteOptions{})
+				gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			})
+
+			ginkgo.By("check if service has been deleted from member clusters", func() {
+				for _, cluster := range clusters {
+					clusterClient := getClusterClient(cluster.Name)
+					gomega.Expect(clusterClient).ShouldNot(gomega.BeNil())
+
+					klog.Infof("Waiting for service(%s/%s) disappear on cluster(%s)", serviceNamespace, serviceName, cluster.Name)
+					err := wait.Poll(pollInterval, pollTimeout, func() (done bool, err error) {
+						_, err = clusterClient.CoreV1().Services(serviceNamespace).Get(context.TODO(), serviceName, metav1.GetOptions{})
 						if err != nil {
 							if errors.IsNotFound(err) {
 								return true, nil
--- test/helper/propagationpolicy.go
+++ test/helper/propagationpolicy.go
@@ -2,24 +2,35 @@ package helper
 
 import (
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	propagationapi "github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1"
 )
 
 // NewPolicyWithSingleDeployment will build a PropagationPolicy object.
-func NewPolicyWithSingleDeployment(namespace string, name string, deployment *appsv1.Deployment, clusters []string) *propagationapi.PropagationPolicy {
+func NewPolicyWithSingleDeployment(namespace, name string, deployment *appsv1.Deployment, clusters []string) *propagationapi.PropagationPolicy {
+	return newPolicy(namespace, name, deployment.APIVersion, deployment.Kind, deployment.Name, clusters)
+}
+
+// NewPolicyWithSingleService will build a PropagationPolicy object.
+func NewPolicyWithSingleService(namespace, name string, service *corev1.Service, clusters []string) *propagationapi.PropagationPolicy {
+	return newPolicy(namespace, name, service.APIVersion, service.Kind, service.Name, clusters)
+}
+
+// newPolicy will build a PropagationPolicy object.
+func newPolicy(namespace, policyName, apiVersion, kind, resourceName string, clusters []string) *propagationapi.PropagationPolicy {
 	return &propagationapi.PropagationPolicy{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: namespace,
-			Name:      name,
+			Name:      policyName,
 		},
 		Spec: propagationapi.PropagationSpec{
 			ResourceSelectors: []propagationapi.ResourceSelector{
 				{
-					APIVersion: deployment.APIVersion,
-					Kind:       deployment.Kind,
-					Name:       deployment.Name,
+					APIVersion: apiVersion,
+					Kind:       kind,
+					Name:       resourceName,
 				},
 			},
 			Placement: propagationapi.Placement{
