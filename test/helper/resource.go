eaec581d40c20772eb902fa44c930bd828ba4c78
changzhen <changzhen5@huawei.com>
2021-03-20 17:41:48 +0800

    add basic crd propagation test
    Signed-off-by: changzhen <changzhen5@huawei.com>

--- test/helper/resource.go
+++ test/helper/resource.go
@@ -3,6 +3,7 @@ package helper
 import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/utils/pointer"
@@ -91,6 +92,87 @@ func NewPod(namespace string, name string) *corev1.Pod {
 					},
 				},
 			},
+		},
+	}
+}
+
+// NewCustomResourceDefinition will build a CRD object.
+func NewCustomResourceDefinition(scope apiextensionsv1.ResourceScope) *apiextensionsv1.CustomResourceDefinition {
+	return &apiextensionsv1.CustomResourceDefinition{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apiextensions.k8s.io/v1",
+			Kind:       "CustomResourceDefinition",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "foos.example.karmada.io",
+		},
+		Spec: apiextensionsv1.CustomResourceDefinitionSpec{
+			Group: "example.karmada.io",
+			Names: apiextensionsv1.CustomResourceDefinitionNames{
+				Kind:     "Foo",
+				ListKind: "FooList",
+				Plural:   "foos",
+				Singular: "foo",
+			},
+			Scope: scope,
+			Versions: []apiextensionsv1.CustomResourceDefinitionVersion{
+				{
+					Name: "v1alpha1",
+					Schema: &apiextensionsv1.CustomResourceValidation{
+						OpenAPIV3Schema: &apiextensionsv1.JSONSchemaProps{
+							Properties: map[string]apiextensionsv1.JSONSchemaProps{
+								"apiVersion": {Type: "string"},
+								"kind":       {Type: "string"},
+								"metadata":   {Type: "object"},
+								"spec": {
+									Properties: map[string]apiextensionsv1.JSONSchemaProps{
+										"clusters": {
+											Items: &apiextensionsv1.JSONSchemaPropsOrArray{
+												Schema: &apiextensionsv1.JSONSchemaProps{
+													Properties: map[string]apiextensionsv1.JSONSchemaProps{
+														"name": {Type: "string"},
+													},
+													Required: []string{"name"},
+													Type:     "object",
+												},
+											},
+											Type: "array",
+										},
+										"resource": {
+											Properties: map[string]apiextensionsv1.JSONSchemaProps{
+												"apiVersion":      {Type: "string"},
+												"kind":            {Type: "string"},
+												"name":            {Type: "string"},
+												"namespace":       {Type: "string"},
+												"resourceVersion": {Type: "string"},
+											},
+											Required: []string{"apiVersion", "kind", "name"},
+											Type:     "object",
+										},
+									},
+									Required: []string{"resource"},
+									Type:     "object",
+								},
+							},
+							Required: []string{"spec"},
+							Type:     "object",
+						},
+					},
+					Served:  true,
+					Storage: true,
+					Subresources: &apiextensionsv1.CustomResourceSubresources{
+						Status: &apiextensionsv1.CustomResourceSubresourceStatus{},
+					},
+				},
+			},
+		},
+		Status: apiextensionsv1.CustomResourceDefinitionStatus{
+			AcceptedNames: apiextensionsv1.CustomResourceDefinitionNames{
+				Kind:   "",
+				Plural: "",
+			},
+			Conditions:     []apiextensionsv1.CustomResourceDefinitionCondition{},
+			StoredVersions: []string{},
 		},
 	}
 }
