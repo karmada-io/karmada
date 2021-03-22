7d80385f382fd19586746e7f23450e07ba8f632e
changzhen <changzhen5@huawei.com>
2021-03-22 21:34:09 +0800

    add basic namespaced scope cr propagation test
    Signed-off-by: changzhen <changzhen5@huawei.com>

--- test/helper/propagationpolicy.go
+++ test/helper/propagationpolicy.go
@@ -5,41 +5,46 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	propagationapi "github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1"
+	policyv1alpha1 "github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1"
 )
 
 // NewPolicyWithSingleDeployment will build a PropagationPolicy object.
-func NewPolicyWithSingleDeployment(namespace, name string, deployment *appsv1.Deployment, clusters []string) *propagationapi.PropagationPolicy {
+func NewPolicyWithSingleDeployment(namespace, name string, deployment *appsv1.Deployment, clusters []string) *policyv1alpha1.PropagationPolicy {
 	return newPolicy(namespace, name, deployment.APIVersion, deployment.Kind, deployment.Name, clusters)
 }
 
 // NewPolicyWithSingleService will build a PropagationPolicy object.
-func NewPolicyWithSingleService(namespace, name string, service *corev1.Service, clusters []string) *propagationapi.PropagationPolicy {
+func NewPolicyWithSingleService(namespace, name string, service *corev1.Service, clusters []string) *policyv1alpha1.PropagationPolicy {
 	return newPolicy(namespace, name, service.APIVersion, service.Kind, service.Name, clusters)
 }
 
 // NewPolicyWithSinglePod will build a PropagationPolicy object.
-func NewPolicyWithSinglePod(namespace, name string, pod *corev1.Pod, clusters []string) *propagationapi.PropagationPolicy {
+func NewPolicyWithSinglePod(namespace, name string, pod *corev1.Pod, clusters []string) *policyv1alpha1.PropagationPolicy {
 	return newPolicy(namespace, name, pod.APIVersion, pod.Kind, pod.Name, clusters)
 }
 
+// NewPolicyWithSingleCR will build a PropagationPolicy object.
+func NewPolicyWithSingleCR(namespace, name, crAPIVersion, crKind, crName string, clusters []string) *policyv1alpha1.PropagationPolicy {
+	return newPolicy(namespace, name, crAPIVersion, crKind, crName, clusters)
+}
+
 // newPolicy will build a PropagationPolicy object.
-func newPolicy(namespace, policyName, apiVersion, kind, resourceName string, clusters []string) *propagationapi.PropagationPolicy {
-	return &propagationapi.PropagationPolicy{
+func newPolicy(namespace, policyName, apiVersion, kind, resourceName string, clusters []string) *policyv1alpha1.PropagationPolicy {
+	return &policyv1alpha1.PropagationPolicy{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: namespace,
 			Name:      policyName,
 		},
-		Spec: propagationapi.PropagationSpec{
-			ResourceSelectors: []propagationapi.ResourceSelector{
+		Spec: policyv1alpha1.PropagationSpec{
+			ResourceSelectors: []policyv1alpha1.ResourceSelector{
 				{
 					APIVersion: apiVersion,
 					Kind:       kind,
 					Name:       resourceName,
 				},
 			},
-			Placement: propagationapi.Placement{
-				ClusterAffinity: &propagationapi.ClusterAffinity{
+			Placement: policyv1alpha1.Placement{
+				ClusterAffinity: &policyv1alpha1.ClusterAffinity{
 					ClusterNames: clusters,
 				},
 			},
