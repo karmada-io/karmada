854cac6e729d9f681ed6a038371832d594ae8950
wawa0210 <xiaozhang0210@hotmail.com>
2021-10-19 20:06:27 +0800

    refactor execute space event
    Signed-off-by: wawa0210 <xiaozhang0210@hotmail.com>

---
+++ pkg/apis/cluster/v1alpha1/events.go
@@ -0,0 +1,9 @@
+package v1alpha1
+
+// Define events for execute space objects.
+const (
+	// EventReasonCreateExecutionSpaceFailed indicates that create execution space failed.
+	EventReasonCreateExecutionSpaceFailed = "CreateExecutionSpaceFailed"
+	// EventReasonRemoveExecutionSpaceFailed indicates that remove execution space failed.
+	EventReasonRemoveExecutionSpaceFailed = "RemoveExecutionSpaceFailed"
+)
--- pkg/controllers/cluster/cluster_controller.go
+++ pkg/controllers/cluster/cluster_controller.go
@@ -33,9 +33,7 @@ const (
 	// sleep between retrying cluster health updates.
 	MonitorRetrySleepTime = 20 * time.Millisecond
 	// HealthUpdateRetry controls the number of retries of writing cluster health update.
-	HealthUpdateRetry                         = 5
-	eventReasonCreateExecutionNamespaceFailed = "CreateExecutionNamespaceFailed"
-	eventReasonRemoveExecutionNamespaceFailed = "RemoveExecutionNamespaceFailed"
+	HealthUpdateRetry = 5
 )
 
 // Controller is to sync Cluster.
@@ -115,7 +113,7 @@ func (c *Controller) syncCluster(cluster *clusterv1alpha1.Cluster) (controllerru
 	// create execution space
 	err := c.createExecutionSpace(cluster)
 	if err != nil {
-		c.EventRecorder.Event(cluster, corev1.EventTypeWarning, fmt.Sprintf("Failed %s", eventReasonRemoveExecutionNamespaceFailed), err.Error())
+		c.EventRecorder.Event(cluster, corev1.EventTypeWarning, fmt.Sprintf("Failed %s", clusterv1alpha1.EventReasonRemoveExecutionSpaceFailed), err.Error())
 		return controllerruntime.Result{Requeue: true}, err
 	}
 
@@ -130,7 +128,7 @@ func (c *Controller) removeCluster(cluster *clusterv1alpha1.Cluster) (controller
 	}
 	if err != nil {
 		klog.Errorf("Failed to remove execution space %v, err is %v", cluster.Name, err)
-		c.EventRecorder.Event(cluster, corev1.EventTypeWarning, fmt.Sprintf("Failed %s", eventReasonCreateExecutionNamespaceFailed), err.Error())
+		c.EventRecorder.Event(cluster, corev1.EventTypeWarning, fmt.Sprintf("Failed %s", clusterv1alpha1.EventReasonCreateExecutionSpaceFailed), err.Error())
 		return controllerruntime.Result{Requeue: true}, err
 	}
 
