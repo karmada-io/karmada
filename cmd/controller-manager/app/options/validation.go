10ac13d3fe4886c0abf139b89b64d9650dded576
charlie <qianglin98@qq.com>
2021-11-13 00:32:19 +0800

    add validation test for controller-manager and refactor the validation of controller-manager
    Signed-off-by: charlie <qianglin98@qq.com>

--- cmd/controller-manager/app/options/validation.go
+++ cmd/controller-manager/app/options/validation.go
@@ -1,84 +1,38 @@
 package options
 
 import (
-	"fmt"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 
 	"github.com/karmada-io/karmada/pkg/util"
 )
 
-func validateSkippedResourceConfig(opts *Options) []error {
-	var errs []error
-	skippedResourceConfig := util.NewSkippedResourceConfig()
-	if err := skippedResourceConfig.Parse(opts.SkippedPropagatingAPIs); err != nil {
-		errs = append(errs, err)
-	}
-
-	return errs
-}
-
-func validateSecurePort(opts *Options) []error {
-	var errs []error
-	if opts.SecurePort < 0 || opts.SecurePort > 65535 {
-		errs = append(errs, fmt.Errorf("--secure-port %v must be between 0 and 65535 inclusive. ", opts.SecurePort))
-	}
-	return errs
-}
-
-func validateClusterStatusUpdateDuration(opts *Options) []error {
-	var errs []error
-
-	if opts.ClusterStatusUpdateFrequency.Duration <= 0 {
-		errs = append(errs, fmt.Errorf("--cluster-status-update-frequency %v must be greater than 0. ", opts.ClusterStatusUpdateFrequency.Duration))
-	}
-	return errs
-}
-
-func validateClusterLeaseDuration(opts *Options) []error {
-	var errs []error
-
-	if opts.ClusterLeaseDuration.Duration <= 0 {
-		errs = append(errs, fmt.Errorf("--cluster-lease-duration %v must be greater than 0. ", opts.ClusterLeaseDuration.Duration))
-	}
-	return errs
-}
-
-func validateClusterMonitorPeriod(opts *Options) []error {
-	var errs []error
-
-	if opts.ClusterMonitorPeriod.Duration <= 0 {
-		errs = append(errs, fmt.Errorf("--cluster-monitor-period %v must be greater than 0. ", opts.ClusterMonitorPeriod.Duration))
-	}
-	return errs
-}
-
-func validateClusterMonitorGracePeriod(opts *Options) []error {
-	var errs []error
-
-	if opts.ClusterMonitorGracePeriod.Duration <= 0 {
-		errs = append(errs, fmt.Errorf("--cluster-monitor-grace-period %v must be greater than 0. ", opts.ClusterMonitorGracePeriod.Duration))
-	}
-	return errs
-}
-
-func validateClusterStartupGracePeriod(opts *Options) []error {
-	var errs []error
-
-	if opts.ClusterStartupGracePeriod.Duration <= 0 {
-		errs = append(errs, fmt.Errorf("--cluster-startup-grace-period %v must be greater than 0. ", opts.ClusterStartupGracePeriod.Duration))
-	}
-	return errs
-}
-
 // Validate checks Options and return a slice of found errs.
-func (o *Options) Validate() []error {
-	var errs []error
-	errs = append(errs, validateSkippedResourceConfig(o)...)
-	errs = append(errs, validateSecurePort(o)...)
-	errs = append(errs, validateClusterStatusUpdateDuration(o)...)
-	errs = append(errs, validateClusterLeaseDuration(o)...)
-	errs = append(errs, validateClusterMonitorPeriod(o)...)
-	errs = append(errs, validateClusterMonitorGracePeriod(o)...)
-	errs = append(errs, validateClusterStartupGracePeriod(o)...)
+func (o *Options) Validate() field.ErrorList {
+	errs := field.ErrorList{}
+	newPath := field.NewPath("Options")
+
+	skippedResourceConfig := util.NewSkippedResourceConfig()
+	if err := skippedResourceConfig.Parse(o.SkippedPropagatingAPIs); err != nil {
+		errs = append(errs, field.Invalid(newPath.Child("SkippedPropagatingAPIs"), o.SkippedPropagatingAPIs, "Invalid API string"))
+	}
+	if o.SecurePort < 0 || o.SecurePort > 65535 {
+		errs = append(errs, field.Invalid(newPath.Child("SecurePort"), o.SecurePort, "must be between 0 and 65535 inclusive"))
+	}
+	if o.ClusterStatusUpdateFrequency.Duration <= 0 {
+		errs = append(errs, field.Invalid(newPath.Child("ClusterStatusUpdateFrequency"), o.ClusterStatusUpdateFrequency, "must be greater than 0"))
+	}
+	if o.ClusterLeaseDuration.Duration <= 0 {
+		errs = append(errs, field.Invalid(newPath.Child("ClusterLeaseDuration"), o.ClusterLeaseDuration, "must be greater than 0"))
+	}
+	if o.ClusterMonitorPeriod.Duration <= 0 {
+		errs = append(errs, field.Invalid(newPath.Child("ClusterMonitorPeriod"), o.ClusterMonitorPeriod, "must be greater than 0"))
+	}
+	if o.ClusterMonitorGracePeriod.Duration <= 0 {
+		errs = append(errs, field.Invalid(newPath.Child("ClusterMonitorGracePeriod"), o.ClusterMonitorGracePeriod, "must be greater than 0"))
+	}
+	if o.ClusterStartupGracePeriod.Duration <= 0 {
+		errs = append(errs, field.Invalid(newPath.Child("ClusterStartupGracePeriod"), o.ClusterStartupGracePeriod, "must be greater than 0"))
+	}
 
 	return errs
 }
---
+++ cmd/controller-manager/app/options/validation_test.go
@@ -0,0 +1,103 @@
+package options
+
+import (
+	"testing"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+// a callback function to modify options
+type ModifyOptions func(option *Options)
+
+// New an Options with default parameters
+func New(modifyOptions ModifyOptions) Options {
+	option := Options{
+		SkippedPropagatingAPIs:       "cluster.karmada.io;policy.karmada.io;work.karmada.io",
+		SecurePort:                   8090,
+		ClusterStatusUpdateFrequency: metav1.Duration{Duration: 10 * time.Second},
+		ClusterLeaseDuration:         metav1.Duration{Duration: 10 * time.Second},
+		ClusterMonitorPeriod:         metav1.Duration{Duration: 10 * time.Second},
+		ClusterMonitorGracePeriod:    metav1.Duration{Duration: 10 * time.Second},
+		ClusterStartupGracePeriod:    metav1.Duration{Duration: 10 * time.Second},
+	}
+
+	if modifyOptions != nil {
+		modifyOptions(&option)
+	}
+	return option
+}
+
+func TestValidateControllerManagerConfiguration(t *testing.T) {
+	successCases := []Options{
+		New(nil),
+	}
+
+	for _, successCase := range successCases {
+		if errs := successCase.Validate(); len(errs) != 0 {
+			t.Errorf("expected success: %v", errs)
+		}
+	}
+
+	newPath := field.NewPath("Options")
+	testCases := map[string]struct {
+		opt          Options
+		expectedErrs field.ErrorList
+	}{
+		"invalid SkippedPropagatingAPIs": {
+			opt: New(func(options *Options) {
+				options.SkippedPropagatingAPIs = "a/b/c/d?"
+			}),
+			expectedErrs: field.ErrorList{field.Invalid(newPath.Child("SkippedPropagatingAPIs"), "a/b/c/d?", "Invalid API string")},
+		},
+		"invalid SecurePort": {
+			opt: New(func(options *Options) {
+				options.SecurePort = -10
+			}),
+			expectedErrs: field.ErrorList{field.Invalid(newPath.Child("SecurePort"), -10, "must be between 0 and 65535 inclusive")},
+		},
+		"invalid ClusterStatusUpdateFrequency": {
+			opt: New(func(options *Options) {
+				options.ClusterStatusUpdateFrequency.Duration = -10 * time.Second
+			}),
+			expectedErrs: field.ErrorList{field.Invalid(newPath.Child("ClusterStatusUpdateFrequency"), metav1.Duration{Duration: -10 * time.Second}, "must be greater than 0")},
+		},
+		"invalid ClusterLeaseDuration": {
+			opt: New(func(options *Options) {
+				options.ClusterLeaseDuration.Duration = -40 * time.Second
+			}),
+			expectedErrs: field.ErrorList{field.Invalid(newPath.Child("ClusterLeaseDuration"), metav1.Duration{Duration: -40 * time.Second}, "must be greater than 0")},
+		},
+		"invalid ClusterMonitorPeriod": {
+			opt: New(func(options *Options) {
+				options.ClusterMonitorPeriod.Duration = -40 * time.Second
+			}),
+			expectedErrs: field.ErrorList{field.Invalid(newPath.Child("ClusterMonitorPeriod"), metav1.Duration{Duration: -40 * time.Second}, "must be greater than 0")},
+		},
+		"invalid ClusterMonitorGracePeriod": {
+			opt: New(func(options *Options) {
+				options.ClusterMonitorGracePeriod.Duration = -40 * time.Second
+			}),
+			expectedErrs: field.ErrorList{field.Invalid(newPath.Child("ClusterMonitorGracePeriod"), metav1.Duration{Duration: -40 * time.Second}, "must be greater than 0")},
+		},
+		"invalid ClusterStartupGracePeriod": {
+			opt: New(func(options *Options) {
+				options.ClusterStartupGracePeriod.Duration = 0 * time.Second
+			}),
+			expectedErrs: field.ErrorList{field.Invalid(newPath.Child("ClusterStartupGracePeriod"), metav1.Duration{Duration: 0 * time.Second}, "must be greater than 0")},
+		},
+	}
+
+	for _, testCase := range testCases {
+		errs := testCase.opt.Validate()
+		if len(testCase.expectedErrs) != len(errs) {
+			t.Fatalf("Expected %d errors, got %d errors: %v", len(testCase.expectedErrs), len(errs), errs)
+		}
+		for i, err := range errs {
+			if err.Error() != testCase.expectedErrs[i].Error() {
+				t.Fatalf("Expected error: %s, got %s", testCase.expectedErrs[i], err.Error())
+			}
+		}
+	}
+}
