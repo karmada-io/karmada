73d49ceaa399ad3a6d8af1e5e1fd6a8937b1965f
LIUSIZE <LIUSIZE@kingai.cn>
2021-12-22 12:54:38 +0800

    delete locla-up-karmada use delete-cluster.sh

--- hack/delete-cluster.sh
+++ hack/delete-cluster.sh
@@ -7,7 +7,7 @@ set -o pipefail
 function usage() {
   echo "This script delete a kube cluster by kind."
   echo "Usage: hack/delete-cluster.sh <CLUSTER_NAME> <KUBECONFIG>"
-  echo "Example: hack/delete-cluster.sh host /root/.kube/karmada.config"
+  echo "Example: hack/delete-cluster.sh karmada-host /root/.kube/karmada.config"
 }
 
 if [[ $# -ne 2 ]]; then
@@ -26,9 +26,14 @@ if [[ -z "${KUBECONFIG}" ]]; then
   exit 1
 fi
 
+REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+source "${REPO_ROOT}"/hack/util.sh
+# befor delete cluster, check kind command exist, Ensure that it can be deleted directly after renaming
+util::cmd_must_exist "kind"
+
 # The context name has been changed when creating clusters by 'create-cluster.sh'.
 # This will result in the context can't be removed by kind when deleting a cluster.
 # So, we need to change context name back and let kind take care about it.
+kubectl config use-context "${CLUSTER_NAME}" --kubeconfig="${KUBECONFIG}"
 kubectl config rename-context "${CLUSTER_NAME}" "kind-${CLUSTER_NAME}" --kubeconfig="${KUBECONFIG}"
-
 kind delete cluster --name "${CLUSTER_NAME}" --kubeconfig="${KUBECONFIG}"
