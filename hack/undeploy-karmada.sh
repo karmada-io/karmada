13e01fef6b0e0941dd5d5c0920de28fe45b5f469
lonelyCZ <531187475@qq.com>
2021-12-23 21:27:15 +0800

    cleanup hack/undeploy-karmada.sh
    Signed-off-by: lonelyCZ <531187475@qq.com>

--- hack/undeploy-karmada.sh
+++ hack/undeploy-karmada.sh
@@ -4,32 +4,38 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+
 function usage() {
   echo "This script will remove karmada control plane from a cluster."
   echo "Usage: hack/undeploy-karmada.sh [KUBECONFIG] [CONTEXT_NAME]"
   echo "Example: hack/undeploy-karmada.sh ~/.kube/karmada.config karmada-host"
 }
 
-SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+if [[ $# -ne 2 ]]; then
+  usage
+  exit 1
+fi
+
+# check kube config file existence
+if [[ ! -f "${1}" ]]; then
+  echo -e "ERROR: failed to get kubernetes config file: '${1}', not existed.\n"
+  usage
+  exit 1
+fi
+HOST_CLUSTER_KUBECONFIG=$1
+
+# check context existence
+if ! kubectl config use-context "${2}" --kubeconfig="${HOST_CLUSTER_KUBECONFIG}" > /dev/null 2>&1;
+then
+  echo -e "ERROR: failed to use context: '${2}' not in ${HOST_CLUSTER_KUBECONFIG}. \n"
+  usage
+  exit 1
+fi
+HOST_CLUSTER_NAME=$2
+
 # delete all keys and certificates
 rm -fr "${HOME}/.karmada"
-# set default host cluster's kubeconfig file (created by kind)
-KUBECONFIG_PATH=${KUBECONFIG_PATH:-"${HOME}/.kube"}
-HOST_CLUSTER_KUBECONFIG="${KUBECONFIG_PATH}/karmada.config"
-HOST_CLUSTER_NAME=${HOST_CLUSTER_NAME:-"karmada-host"}
-
-# if provider the custom kubeconfig and context name
-if [[ -f "${1:-}" ]]; then
-  if ! kubectl config get-contexts "${2:-}" --kubeconfig="${1}" > /dev/null 2>&1;
-  then
-    echo -e "ERROR: failed to get context: '${2:-}' not in ${1}. \n"
-    usage
-    exit 1
-  else
-    HOST_CLUSTER_KUBECONFIG=${1:-}
-    HOST_CLUSTER_NAME=${2:-}
-  fi
-fi
 
 kubectl config use-context "${HOST_CLUSTER_NAME}" --kubeconfig="${HOST_CLUSTER_KUBECONFIG}"
 
