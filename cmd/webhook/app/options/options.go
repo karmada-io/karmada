1b468431fece606f571924a38a5adba7183e8d40
carlory <baofa.fan@daocloud.io>
2022-01-19 10:35:39 +0800

    introduce --tls-min-version flag to karmada-webhook
    Co-authored-by: Hongcai Ren <renhongcai@huawei.com>
    Signed-off-by: carlory <baofa.fan@daocloud.io>

--- cmd/webhook/app/options/options.go
+++ cmd/webhook/app/options/options.go
@@ -5,9 +5,10 @@ import (
 )
 
 const (
-	defaultBindAddress = "0.0.0.0"
-	defaultPort        = 8443
-	defaultCertDir     = "/tmp/k8s-webhook-server/serving-certs"
+	defaultBindAddress   = "0.0.0.0"
+	defaultPort          = 8443
+	defaultCertDir       = "/tmp/k8s-webhook-server/serving-certs"
+	defaultTLSMinVersion = "1.3"
 )
 
 // Options contains everything necessary to create and run webhook server.
@@ -22,6 +23,11 @@ type Options struct {
 	// if not set, webhook server would look up the server key and certificate in {TempDir}/k8s-webhook-server/serving-certs.
 	// The server key and certificate must be named `tls.key` and `tls.crt`, respectively.
 	CertDir string
+	// TLSMinVersion is the minimum version of TLS supported. Possible values: 1.0, 1.1, 1.2, 1.3.
+	// Some environments have automated security scans that trigger on TLS versions or insecure cipher suites, and
+	// setting TLS to 1.3 would solve both problems.
+	// Defaults to 1.3.
+	TLSMinVersion string
 	// KubeAPIQPS is the QPS to use while talking with karmada-apiserver.
 	KubeAPIQPS float32
 	// KubeAPIBurst is the burst to allow while talking with karmada-apiserver.
@@ -41,6 +47,7 @@ func (o *Options) AddFlags(flags *pflag.FlagSet) {
 		"The secure port on which to serve HTTPS.")
 	flags.StringVar(&o.CertDir, "cert-dir", defaultCertDir,
 		"The directory that contains the server key(named tls.key) and certificate(named tls.crt).")
+	flags.StringVar(&o.TLSMinVersion, "tls-min-version", defaultTLSMinVersion, "Minimum TLS version supported. Possible values: 1.0, 1.1, 1.2, 1.3.")
 	flags.Float32Var(&o.KubeAPIQPS, "kube-api-qps", 40.0, "QPS to use while talking with karmada-apiserver. Doesn't cover events and node heartbeat apis which rate limiting is controlled by a different set of flags.")
 	flags.IntVar(&o.KubeAPIBurst, "kube-api-burst", 60, "Burst to use while talking with karmada-apiserver. Doesn't cover events and node heartbeat apis which rate limiting is controlled by a different set of flags.")
 }
