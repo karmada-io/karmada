eeb0f28244b13bd1dbc7190de43c6f528b6b5b28
Alan Zhu <zg.zhu@daocloud.io>
2022-01-20 18:04:53 +0800

    karmada init: add more message for easy debug  when nodePort exist
    Signed-off-by: Alan Zhu <zg.zhu@daocloud.io>

--- pkg/karmadactl/cmdinit/kubernetes/deploy.go
+++ pkg/karmadactl/cmdinit/kubernetes/deploy.go
@@ -109,7 +109,7 @@ func (i *CommandInitOption) Complete() error {
 	i.KubeClientSet = clientSet
 
 	if !i.isNodePortExist() {
-		return fmt.Errorf("nodePort %v already exist", i.KarmadaAPIServerNodePort)
+		return fmt.Errorf("nodePort of karmada apiserver %v already exist", i.KarmadaAPIServerNodePort)
 	}
 
 	if i.EtcdStorageMode == "hostPath" && i.EtcdNodeSelectorLabels == "" {
