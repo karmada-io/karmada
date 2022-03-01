bfee7c45cf40ddab38e611f01bc9809c5f4a770f
AllenZMC <zhongming.chang@daocloud.io>
2022-03-02 00:23:04 +0800

    improve test coverage
    Signed-off-by: AllenZMC <zhongming.chang@daocloud.io>

--- pkg/util/binding_test.go
+++ pkg/util/binding_test.go
@@ -1,10 +1,13 @@
 package util
 
 import (
+	"reflect"
 	"testing"
 
+	policyv1alpha1 "github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1"
 	workv1alpha2 "github.com/karmada-io/karmada/pkg/apis/work/v1alpha2"
 	testhelper "github.com/karmada-io/karmada/test/helper"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -156,6 +159,200 @@ func TestDivideReplicasByTargetCluster(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := DivideReplicasByTargetCluster(tt.args.clusters, tt.args.sum); !testhelper.IsScheduleResultEqual(got, tt.want) {
 				t.Errorf("DivideReplicasByTargetCluster() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetBindingClusterNames(t *testing.T) {
+	tests := []struct {
+		name     string
+		binding  *workv1alpha2.ResourceBinding
+		expected []string
+	}{
+		{
+			name: "nil",
+			binding: &workv1alpha2.ResourceBinding{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "demo-name",
+					Namespace: "demo-ns",
+				},
+				Spec: workv1alpha2.ResourceBindingSpec{
+					Clusters: []workv1alpha2.TargetCluster{},
+				},
+				Status: workv1alpha2.ResourceBindingStatus{},
+			},
+			expected: nil,
+		},
+		{
+			name: "not nil",
+			binding: &workv1alpha2.ResourceBinding{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "demo-name",
+					Namespace: "demo-ns",
+				},
+				Spec: workv1alpha2.ResourceBindingSpec{
+					Clusters: []workv1alpha2.TargetCluster{
+						{
+							Name: ClusterMember1,
+						},
+						{
+							Name: ClusterMember2,
+						},
+					},
+				},
+				Status: workv1alpha2.ResourceBindingStatus{},
+			},
+			expected: []string{ClusterMember1, ClusterMember2},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := GetBindingClusterNames(tt.binding)
+			if !reflect.DeepEqual(got, tt.expected) {
+				t.Errorf("GetBindingClusterNames() = %v, want %v", got, tt.expected)
+			}
+		})
+	}
+}
+
+func TestIsBindingReplicasChanged(t *testing.T) {
+	tests := []struct {
+		name        string
+		bindingSpec *workv1alpha2.ResourceBindingSpec
+		strategy    *policyv1alpha1.ReplicaSchedulingStrategy
+		expected    bool
+	}{
+		{
+			name:        "nil strategy",
+			bindingSpec: &workv1alpha2.ResourceBindingSpec{},
+			strategy:    nil,
+			expected:    false,
+		},
+		{
+			name:        "empty strategy",
+			bindingSpec: &workv1alpha2.ResourceBindingSpec{},
+			strategy:    &policyv1alpha1.ReplicaSchedulingStrategy{},
+			expected:    false,
+		},
+		{
+			name: "Duplicated strategy and replicas not changed",
+			bindingSpec: &workv1alpha2.ResourceBindingSpec{
+				Replicas: 5,
+				Clusters: []workv1alpha2.TargetCluster{
+					{
+						Name:     ClusterMember1,
+						Replicas: 5,
+					},
+					{
+						Name:     ClusterMember2,
+						Replicas: 5,
+					},
+				}},
+			strategy: &policyv1alpha1.ReplicaSchedulingStrategy{ReplicaSchedulingType: policyv1alpha1.ReplicaSchedulingTypeDuplicated},
+			expected: false,
+		},
+		{
+			name: "Duplicated strategy and replicas changed",
+			bindingSpec: &workv1alpha2.ResourceBindingSpec{
+				Replicas: 5,
+				Clusters: []workv1alpha2.TargetCluster{
+					{
+						Name:     ClusterMember1,
+						Replicas: 3,
+					},
+					{
+						Name:     ClusterMember2,
+						Replicas: 5,
+					},
+				}},
+			strategy: &policyv1alpha1.ReplicaSchedulingStrategy{ReplicaSchedulingType: policyv1alpha1.ReplicaSchedulingTypeDuplicated},
+			expected: true,
+		},
+		{
+			name: "Divided strategy and replicas not changed",
+			bindingSpec: &workv1alpha2.ResourceBindingSpec{
+				Replicas: 5,
+				Clusters: []workv1alpha2.TargetCluster{
+					{
+						Name:     ClusterMember1,
+						Replicas: 2,
+					},
+					{
+						Name:     ClusterMember2,
+						Replicas: 3,
+					},
+				},
+			},
+			strategy: &policyv1alpha1.ReplicaSchedulingStrategy{ReplicaSchedulingType: policyv1alpha1.ReplicaSchedulingTypeDivided},
+			expected: false,
+		},
+		{
+			name: "Divided strategy and replicas changed",
+			bindingSpec: &workv1alpha2.ResourceBindingSpec{
+				Replicas: 5,
+				Clusters: []workv1alpha2.TargetCluster{
+					{
+						Name:     ClusterMember1,
+						Replicas: 3,
+					},
+					{
+						Name:     ClusterMember2,
+						Replicas: 3,
+					},
+				},
+			},
+			strategy: &policyv1alpha1.ReplicaSchedulingStrategy{ReplicaSchedulingType: policyv1alpha1.ReplicaSchedulingTypeDivided},
+			expected: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.bindingSpec == nil {
+				t.FailNow()
+			}
+			got := IsBindingReplicasChanged(tt.bindingSpec, tt.strategy)
+			if got != tt.expected {
+				t.Errorf("IsBindingReplicasChanged() = %v, want %v", got, tt.expected)
+			}
+		})
+	}
+}
+
+func TestGetSumOfReplicas(t *testing.T) {
+	tests := []struct {
+		name     string
+		clusters []workv1alpha2.TargetCluster
+		expected int32
+	}{
+		{
+			name:     "empty",
+			clusters: []workv1alpha2.TargetCluster{},
+			expected: 0,
+		},
+		{
+			name: "not empty",
+			clusters: []workv1alpha2.TargetCluster{
+				{
+					Name:     ClusterMember1,
+					Replicas: 2,
+				},
+				{
+					Name:     ClusterMember2,
+					Replicas: 3,
+				},
+			},
+			expected: 5,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := GetSumOfReplicas(tt.clusters)
+			if got != tt.expected {
+				t.Errorf("GetSumOfReplicas() = %v, want %v", got, tt.expected)
 			}
 		})
 	}
