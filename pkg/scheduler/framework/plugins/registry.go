4e436af66ee00901a34b5a08ce133cfcef4da33c
gf457832386 <13278887558@163.com>
2022-03-29 10:23:38 +0800

    add filter plugin SpreadConstraint to filter cluster
    Signed-off-by: gf457832386 <13278887558@163.com>

--- pkg/scheduler/framework/plugins/registry.go
+++ pkg/scheduler/framework/plugins/registry.go
@@ -5,15 +5,17 @@ import (
 	"github.com/karmada-io/karmada/pkg/scheduler/framework/plugins/apiinstalled"
 	"github.com/karmada-io/karmada/pkg/scheduler/framework/plugins/clusteraffinity"
 	"github.com/karmada-io/karmada/pkg/scheduler/framework/plugins/clusterlocality"
+	"github.com/karmada-io/karmada/pkg/scheduler/framework/plugins/spreadconstraint"
 	"github.com/karmada-io/karmada/pkg/scheduler/framework/plugins/tainttoleration"
 )
 
 // NewPlugins builds all the scheduling plugins.
 func NewPlugins() map[string]framework.Plugin {
 	return map[string]framework.Plugin{
-		clusteraffinity.Name: clusteraffinity.New(),
-		tainttoleration.Name: tainttoleration.New(),
-		apiinstalled.Name:    apiinstalled.New(),
-		clusterlocality.Name: clusterlocality.New(),
+		clusteraffinity.Name:  clusteraffinity.New(),
+		tainttoleration.Name:  tainttoleration.New(),
+		apiinstalled.Name:     apiinstalled.New(),
+		clusterlocality.Name:  clusterlocality.New(),
+		spreadconstraint.Name: spreadconstraint.New(),
 	}
 }
