dafe30c64928298362f6db829faecba9fdb87a79
yingjinhui <yingjinhui@didiglobal.com>
2022-05-13 11:45:24 +0800

    fix test fail on arm64 mac `panic: permission denied`, removing gomonkey
    Signed-off-by: yingjinhui <yingjinhui@didiglobal.com>

--- pkg/scheduler/core/spreadconstraint/group_clusters_test.go
+++ pkg/scheduler/core/spreadconstraint/group_clusters_test.go
@@ -3,8 +3,6 @@ package spreadconstraint
 import (
 	"testing"
 
-	"github.com/agiledragon/gomonkey/v2"
-
 	clusterv1alpha1 "github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1"
 	policyv1alpha1 "github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1"
 	workv1alpha2 "github.com/karmada-io/karmada/pkg/apis/work/v1alpha2"
@@ -173,7 +171,7 @@ func Test_GroupClustersWithScore(t *testing.T) {
 		},
 	}
 
-	patches := gomonkey.ApplyFunc(calAvailableReplicas, func(clusters []*clusterv1alpha1.Cluster, spec *workv1alpha2.ResourceBindingSpec) []workv1alpha2.TargetCluster {
+	calAvailableReplicasFunc := func(clusters []*clusterv1alpha1.Cluster, spec *workv1alpha2.ResourceBindingSpec) []workv1alpha2.TargetCluster {
 		availableTargetClusters := make([]workv1alpha2.TargetCluster, len(clusters))
 
 		for i := range availableTargetClusters {
@@ -182,13 +180,11 @@ func Test_GroupClustersWithScore(t *testing.T) {
 		}
 
 		return availableTargetClusters
-	})
-
-	defer patches.Reset()
+	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			groupInfo := GroupClustersWithScore(tt.args.clustersScore, tt.args.placement, tt.args.spec)
+			groupInfo := GroupClustersWithScore(tt.args.clustersScore, tt.args.placement, tt.args.spec, calAvailableReplicasFunc)
 			for i, cluster := range groupInfo.Clusters {
 				if cluster.Name != tt.want.clusters[i] {
 					t.Errorf("test %s : the clusters aren't sorted", tt.name)
--- pkg/scheduler/core/spreadconstraint/util.go
+++ pkg/scheduler/core/spreadconstraint/util.go
@@ -1,18 +1,9 @@
 package spreadconstraint
 
 import (
-	"context"
-	"fmt"
-	"math"
 	"sort"
 
-	"k8s.io/klog/v2"
-
-	clusterv1alpha1 "github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1"
 	policyv1alpha1 "github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1"
-	workv1alpha2 "github.com/karmada-io/karmada/pkg/apis/work/v1alpha2"
-	estimatorclient "github.com/karmada-io/karmada/pkg/estimator/client"
-	"github.com/karmada-io/karmada/pkg/util"
 )
 
 const (
@@ -23,47 +14,6 @@ const (
 	// InvalidReplicas indicate that don't care about the available resource
 	InvalidReplicas = -1
 )
-
-func calAvailableReplicas(clusters []*clusterv1alpha1.Cluster, spec *workv1alpha2.ResourceBindingSpec) []workv1alpha2.TargetCluster {
-	availableClusters := make([]workv1alpha2.TargetCluster, len(clusters))
-
-	// Set the boundary.
-	for i := range availableClusters {
-		availableClusters[i].Name = clusters[i].Name
-		availableClusters[i].Replicas = math.MaxInt32
-	}
-
-	// Get the minimum value of MaxAvailableReplicas in terms of all estimators.
-	estimators := estimatorclient.GetReplicaEstimators()
-	ctx := context.WithValue(context.TODO(), util.ContextKeyObject,
-		fmt.Sprintf("kind=%s, name=%s/%s", spec.Resource.Kind, spec.Resource.Namespace, spec.Resource.Name))
-	for _, estimator := range estimators {
-		res, err := estimator.MaxAvailableReplicas(ctx, clusters, spec.ReplicaRequirements)
-		if err != nil {
-			klog.Errorf("Max cluster available replicas error: %v", err)
-			continue
-		}
-		for i := range res {
-			if res[i].Replicas == estimatorclient.UnauthenticReplica {
-				continue
-			}
-			if availableClusters[i].Name == res[i].Name && availableClusters[i].Replicas > res[i].Replicas {
-				availableClusters[i].Replicas = res[i].Replicas
-			}
-		}
-	}
-
-	// In most cases, the target cluster max available replicas should not be MaxInt32 unless the workload is best-effort
-	// and the scheduler-estimator has not been enabled. So we set the replicas to spec.Replicas for avoiding overflow.
-	for i := range availableClusters {
-		if availableClusters[i].Replicas == math.MaxInt32 {
-			availableClusters[i].Replicas = spec.Replicas
-		}
-	}
-
-	klog.V(4).Infof("cluster replicas info: %v", availableClusters)
-	return availableClusters
-}
 
 // IsSpreadConstraintExisted judge if the specific field is existed in the spread constraints
 func IsSpreadConstraintExisted(spreadConstraints []policyv1alpha1.SpreadConstraint, field policyv1alpha1.SpreadFieldValue) bool {
