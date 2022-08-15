1374169fe32ff15dde26f76f1d4f288fb14a1874
Poor12 <shentiecheng@huawei.com>
2022-08-15 11:26:30 +0800

    Rename package name of typedmanager
    Signed-off-by: Poor12 <shentiecheng@huawei.com>

--- cmd/controller-manager/app/controllermanager.go
+++ cmd/controller-manager/app/controllermanager.go
@@ -48,7 +48,7 @@ import (
 	"github.com/karmada-io/karmada/pkg/sharedcli/profileflag"
 	"github.com/karmada-io/karmada/pkg/util"
 	"github.com/karmada-io/karmada/pkg/util/fedinformer/genericmanager"
-	"github.com/karmada-io/karmada/pkg/util/fedinformer/typedManager"
+	"github.com/karmada-io/karmada/pkg/util/fedinformer/typedmanager"
 	"github.com/karmada-io/karmada/pkg/util/gclient"
 	"github.com/karmada-io/karmada/pkg/util/helper"
 	"github.com/karmada-io/karmada/pkg/util/objectwatcher"
@@ -255,7 +255,7 @@ func startClusterStatusController(ctx controllerscontext.Context) (enabled bool,
 		KubeClient:                        kubeclientset.NewForConfigOrDie(mgr.GetConfig()),
 		EventRecorder:                     mgr.GetEventRecorderFor(status.ControllerName),
 		PredicateFunc:                     clusterPredicateFunc,
-		TypedInformerManager:              typedManager.GetInstance(),
+		TypedInformerManager:              typedmanager.GetInstance(),
 		GenericInformerManager:            genericmanager.GetInstance(),
 		StopChan:                          stopChan,
 		ClusterClientSetFunc:              util.NewClusterClientSet,
--- pkg/util/fedinformer/typedmanager/single-cluster-manager.go
+++ pkg/util/fedinformer/typedmanager/single-cluster-manager.go
@@ -1,4 +1,4 @@
-package typedManager
+package typedmanager
 
 import (
 	"context"
