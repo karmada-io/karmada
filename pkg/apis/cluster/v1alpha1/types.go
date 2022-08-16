77cf4bed09994921496940d7840274485d64cc92
halfrost <ydz627@gmail.com>
2022-08-15 21:29:59 -0700

    Add cluster resource modeling api
    Signed-off-by: halfrost <ydz627@gmail.com>

--- pkg/apis/cluster/v1alpha1/types.go
+++ pkg/apis/cluster/v1alpha1/types.go
@@ -2,6 +2,7 @@ package v1alpha1
 
 import (
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -14,6 +15,26 @@ const (
 	ResourcePluralCluster = "clusters"
 	// ResourceNamespaceScopedCluster indicates if Cluster is NamespaceScoped.
 	ResourceNamespaceScopedCluster = false
+)
+
+// ResourceName is the name identifying various resources in a ResourceList.
+type ResourceName string
+
+// Resource names must be not more than 63 characters, consisting of upper- or lower-case alphanumeric characters,
+// with the -, _, and . characters allowed anywhere, except the first or last character.
+// The default convention, matching that for annotations, is to use lower-case names, with dashes, rather than
+// camel case, separating compound words.
+// Fully-qualified resource typenames are constructed from a DNS-style subdomain, followed by a slash `/` and a name.
+const (
+	// ResourceCPU in cores. (e,g. 500m = .5 cores)
+	ResourceCPU ResourceName = "cpu"
+	// ResourceMemory in bytes. (e,g. 500Gi = 500GiB = 500 * 1024 * 1024 * 1024)
+	ResourceMemory ResourceName = "memory"
+	// ResourceStorage is volume size, in bytes (e,g. 5Gi = 5GiB = 5 * 1024 * 1024 * 1024)
+	ResourceStorage ResourceName = "storage"
+	// ResourceEphemeralStorage is local ephemeral storage, in bytes. (e,g. 500Gi = 500GiB = 500 * 1024 * 1024 * 1024)
+	// The resource name for ResourceEphemeralStorage is alpha and it can change across releases.
+	ResourceEphemeralStorage ResourceName = "ephemeral-storage"
 )
 
 // +genclient
@@ -116,6 +137,104 @@ type ClusterSpec struct {
 	// any resource that does not tolerate the Taint.
 	// +optional
 	Taints []corev1.Taint `json:"taints,omitempty"`
+
+	// ResourceModels is the list of resource modeling in this cluster. Each modeling quota can be customized by the user.
+	// Modeling name must be one of the following: cpu, memory, storage, ephemeral-storage.
+	// If the user does not define the modeling name and modeling quota, it will be the default model.
+	// The default model grade from 0 to 8.
+	// When grade = 0 or grade = 1, the default model's cpu quota and memory quota is a fix value.
+	// When grade greater than or equal to 2, each default model's cpu quota is [2^(grade-1), 2^grade), 2 <= grade <= 7
+	// Each default model's memory quota is [2^(grade + 2), 2^(grade + 3)), 2 <= grade <= 7
+	// E.g. grade 0 likes this:
+	// - grade: 0
+	//   ranges:
+	//   - name: "cpu"
+	//     min: 0 C
+	//     max: 1 C
+	//   - name: "memory"
+	//     min: 0 GB
+	//     max: 4 GB
+	//
+	// - grade: 1
+	//   ranges:
+	//   - name: "cpu"
+	//     min: 1 C
+	//     max: 2 C
+	//   - name: "memory"
+	//     min: 4 GB
+	//     max: 16 GB
+	//
+	// - grade: 2
+	//   ranges:
+	//   - name: "cpu"
+	//     min: 2 C
+	//     max: 4 C
+	//   - name: "memory"
+	//     min: 16 GB
+	//     max: 32 GB
+	//
+	// - grade: 7
+	//   range:
+	//   - name: "cpu"
+	//     min: 64 C
+	//     max: 128 C
+	//   - name: "memory"
+	//     min: 512 GB
+	//     max: 1024 GB
+	//
+	// grade 8, the last one likes below. No matter what Max value you pass,
+	// the meaning of Max value in this grade is infinite. You can pass any number greater than Min value.
+	// - grade: 8
+	//   range:
+	//   - name: "cpu"
+	//     min: 128 C
+	//     max: MAXINT
+	//   - name: "memory"
+	//     min: 1024 GB
+	//     max: MAXINT
+	//
+	// +optional
+	ResourceModels []ResourceModel `json:"resourceModels,omitempty"`
+}
+
+// ResourceModel describes the modeling that you want to statistics.
+type ResourceModel struct {
+	// Grade is the index for the resource modeling.
+	// +optional
+	Grade int `json:"grade,omitempty"`
+
+	// Ranges describes the resource quota ranges.
+	// +optional
+	Ranges []ResourceModelRange `json:"ranges,omitempty"`
+}
+
+// ResourceModelRange describes the detail of each modeling quota that ranges from min to max.
+// Please pay attention, by default, the value of min can be inclusive, and the value of max cannot be inclusive.
+// E.g. in an interval, min = 2, max =10 is set, which means the interval [2,10).
+// This rule ensure that all intervals have the same meaning. If the last interval is +∞,
+// it is definitely unreachable. Therefore, we define the right interval as the open interval.
+// For a valid interval, the value on the right is greater than the value on the left,
+// in other words, max must be greater than min.
+// It is strongly recommended that the [Min, Max) of all ResourceModelRanges can make a continuous interval.
+type ResourceModelRange struct {
+	// Name is the name for the resource that you want to categorize.
+	// +optional
+	Name ResourceName `json:"name,omitempty"`
+
+	// Min is the minimum amount of this resource represented by resource name.
+	// Note: The Min value of first grade(usually 0) always acts as zero.
+	// E.g. [1,2) equal to [0,2).
+	// +optional
+	Min resource.Quantity `json:"min,omitempty"`
+
+	// Max is the maximum amount of this resource represented by resource name.
+	// Special Instructions, for the last ResourceModelRange, which no matter what Max value you pass,
+	// the meaning is infinite. Because for the last item,
+	// any ResourceModelRange's quota larger than Min will be classified to the last one.
+	// Of course, the value of the Max field is always greater than the value of the Min field.
+	// It should be true in any case.
+	// +optional
+	Max resource.Quantity `json:"max,omitempty"`
 }
 
 const (
@@ -215,14 +334,32 @@ type ResourceSummary struct {
 	// Total amount of allocatable resources on all nodes.
 	// +optional
 	Allocatable corev1.ResourceList `json:"allocatable,omitempty"`
+
 	// Allocating represents the resources of a cluster that are pending for scheduling.
 	// Total amount of required resources of all Pods that are waiting for scheduling.
 	// +optional
 	Allocating corev1.ResourceList `json:"allocating,omitempty"`
+
 	// Allocated represents the resources of a cluster that have been scheduled.
 	// Total amount of required resources of all Pods that have been scheduled to nodes.
 	// +optional
 	Allocated corev1.ResourceList `json:"allocated,omitempty"`
+
+	// AllocatableModelings represents the statistical resource modeling.
+	// +optional
+	AllocatableModelings []AllocatableModeling `json:"allocatableModelings,omitempty"`
+}
+
+// AllocatableModeling represents the number of nodes in which allocatable resources in a specific resource model grade.
+// E.g. AllocatableModeling{Grade: 2, Count: 10} means 10 nodes belong to resource model in grade 2.
+type AllocatableModeling struct {
+	// Grade is the index of ResourceModel.
+	// +optional
+	Grade int `json:"grade,omitempty"`
+
+	// Count is the number of nodes that own the resources delineated by this modeling.
+	// +optional
+	Count int `json:"count,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
