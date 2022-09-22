19bc00f6e976095b70cb6c20423411ef218eadbd
jwcesign <jiangwei115@huawei.com>
2022-09-22 17:11:46 +0800

    Fix cherry pick script to skip verify certificate
    Signed-off-by: jwcesign <jiangwei115@huawei.com>

--- hack/cherry_pick_pull.sh
+++ hack/cherry_pick_pull.sh
@@ -162,7 +162,7 @@ gitamcleanup=true
 for pull in "${PULLS[@]}"; do
   echo "+++ Downloading patch to /tmp/${pull}.patch (in case you need to do this again)"
 
-  curl -o "/tmp/${pull}.patch" -sSL "https://github.com/${MAIN_REPO_ORG}/${MAIN_REPO_NAME}/pull/${pull}.patch"
+  curl -o "/tmp/${pull}.patch" -sSL "https://github.com/${MAIN_REPO_ORG}/${MAIN_REPO_NAME}/pull/${pull}.patch" -k
   echo
   echo "+++ About to attempt cherry pick of PR. To reattempt:"
   echo "  $ git am -3 /tmp/${pull}.patch"
