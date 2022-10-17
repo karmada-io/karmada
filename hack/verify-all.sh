a7f4305fe7e6582f168ac3b4520b2b4b599f13ea
raymondmiaochaoyue <raymondmiaochaoyue@didiglobal.com>
2022-10-17 15:12:40 +0800

    reorder `make verify`
    Signed-off-by: raymondmiaochaoyue <raymondmiaochaoyue@didiglobal.com>

--- hack/verify-all.sh
+++ hack/verify-all.sh
@@ -6,10 +6,16 @@ set -o pipefail
 
 REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 
-bash "$REPO_ROOT/hack/verify-codegen.sh"
-bash "$REPO_ROOT/hack/verify-crdgen.sh"
-bash "$REPO_ROOT/hack/verify-staticcheck.sh"
+# Show progress
+set -x
+
+# Orders are determined by two factors:
+# (1) Less Execution time item should be executed first.
+# (2) More likely to fail item should be executed first.
+bash "$REPO_ROOT/hack/verify-lifted.sh"
 bash "$REPO_ROOT/hack/verify-import-aliases.sh"
+bash "$REPO_ROOT/hack/verify-staticcheck.sh"
 bash "$REPO_ROOT/hack/verify-vendor.sh"
 bash "$REPO_ROOT/hack/verify-swagger-docs.sh"
-bash "$REPO_ROOT/hack/verify-lifted.sh"
+bash "$REPO_ROOT/hack/verify-crdgen.sh"
+bash "$REPO_ROOT/hack/verify-codegen.sh"
