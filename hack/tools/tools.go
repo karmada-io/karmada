c0138e7a3d721c160c5c5822244281a363d2c078
RainbowMango <qdurenhongcai@gmail.com>
2022-10-25 18:09:48 +0800

    install mockgen from vendor
    Signed-off-by: RainbowMango <qdurenhongcai@gmail.com>

--- hack/tools/tools.go
+++ hack/tools/tools.go
@@ -5,6 +5,7 @@ package tools
 
 import (
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
+	_ "github.com/golang/mock/mockgen"
 	_ "github.com/onsi/ginkgo/v2/ginkgo"
 	_ "github.com/vektra/mockery/v2"
 	_ "golang.org/x/tools/cmd/goimports"
