5cc6d813ab7a6952fbbc47e1aacfc78663f7331b
yingjinhui <yingjinhui@didiglobal.com>
2022-11-14 17:30:05 +0800

    fix error when convert empty lua table
    Signed-off-by: yingjinhui <yingjinhui@didiglobal.com>

--- pkg/resourceinterpreter/configurableinterpreter/luavm/lua_convert.go
+++ pkg/resourceinterpreter/configurableinterpreter/luavm/lua_convert.go
@@ -16,6 +16,60 @@ func ConvertLuaResultInto(luaResult lua.LValue, obj interface{}) error {
 	if err != nil {
 		return fmt.Errorf("obj is not pointer")
 	}
+
+	// For example, `GetReplicas` returns requirement with empty:
+	//     {
+	//         nodeClaim: {},
+	//         resourceRequest: {
+	//             cpu: "100m"
+	//         }
+	//     }
+	// Luajson encodes it to
+	//     {"nodeClaim": [], "resourceRequest": {"cpu": "100m"}}
+	//
+	// While go json fails to unmarshal `[]` to ReplicaRequirements.NodeClaim object.
+	// ReplicaRequirements object.
+	//
+	// Here we handle it as follows:
+	//   1. Walk the object (lua table), delete the key with empty value (`nodeClaim` in this example):
+	//     {
+	//         resourceRequest: {
+	//             cpu: "100m"
+	//         }
+	//     }
+	//   2. Encode the object with luajson to be:
+	//     {"resourceRequest": {"cpu": "100m"}}
+	//   4. Finally, unmarshal the new json to object, get
+	//     {
+	//         resourceRequest: {
+	//             cpu: "100m"
+	//         }
+	//     }
+	isEmptyDic := func(v *lua.LTable) bool {
+		count := 0
+		v.ForEach(func(lua.LValue, lua.LValue) {
+			count++
+		})
+		return count == 0
+	}
+
+	var walkValue func(v lua.LValue)
+	walkValue = func(v lua.LValue) {
+		if t, ok := v.(*lua.LTable); ok {
+			t.ForEach(func(key lua.LValue, value lua.LValue) {
+				if tt, ok := value.(*lua.LTable); ok {
+					if isEmptyDic(tt) {
+						// set nil to delete key
+						t.RawSetH(key, lua.LNil)
+					} else {
+						walkValue(value)
+					}
+				}
+			})
+		}
+	}
+	walkValue(luaResult)
+
 	jsonBytes, err := luajson.Encode(luaResult)
 	if err != nil {
 		return fmt.Errorf("json Encode obj eroor %v", err)
