88e3c2add1d09dc4ac4dbcdb67808c058f2915e2
RainbowMango <qdurenhongcai@gmail.com>
2023-01-19 16:57:21 +0800

    simplify context tests
    Signed-off-by: RainbowMango <qdurenhongcai@gmail.com>

--- pkg/controllers/context/context_test.go
+++ pkg/controllers/context/context_test.go
@@ -6,103 +6,73 @@ import (
 	"k8s.io/apimachinery/pkg/util/sets"
 )
 
-type args struct {
-	controllerName               string
-	disabledByDefaultControllers []string
-}
-
 func TestContext_IsControllerEnabled(t *testing.T) {
 	tests := []struct {
-		name     string
-		opts     Options
-		args     args
-		expected bool
+		name                         string
+		controllerName               string
+		controllers                  []string
+		disabledByDefaultControllers []string
+		expected                     bool
 	}{
 		{
-			name: "on by name",
-			args: args{
-				controllerName:               "bravo",
-				disabledByDefaultControllers: []string{"delta", "echo"},
-			},
-			opts: Options{
-				Controllers: []string{"alpha", "bravo", "-charlie"},
-			},
-			expected: true,
+			name:                         "on by name",
+			controllerName:               "bravo",
+			disabledByDefaultControllers: []string{"delta", "echo"},
+			controllers:                  []string{"alpha", "bravo", "-charlie"}, // --controllers=alpha,bravo,-charlie
+			expected:                     true,
 		},
 		{
-			name: "off by name",
-			args: args{
-				controllerName:               "charlie",
-				disabledByDefaultControllers: []string{"delta", "echo"},
-			},
-			opts: Options{
-				Controllers: []string{"alpha", "bravo", "-charlie"},
-			},
-			expected: false,
+			name:                         "off by name",
+			controllerName:               "charlie",
+			disabledByDefaultControllers: []string{"delta", "echo"},
+			controllers:                  []string{"alpha", "bravo", "-charlie"}, // --controllers=alpha,bravo,-charlie
+			expected:                     false,
 		},
 		{
-			name: "on by default",
-			args: args{
-				controllerName:               "alpha",
-				disabledByDefaultControllers: []string{"delta", "echo"},
-			},
-			opts: Options{
-				Controllers: []string{"*"},
-			},
-			expected: true,
+			name:                         "on by default",
+			controllerName:               "alpha",
+			disabledByDefaultControllers: []string{"delta", "echo"},
+			controllers:                  []string{"*"}, // --controllers=*
+			expected:                     true,
 		},
 		{
-			name: "off by default",
-			args: args{
-				controllerName:               "delta",
-				disabledByDefaultControllers: []string{"delta", "echo"},
-			},
-			opts: Options{
-				Controllers: []string{"*"},
-			},
-			expected: false,
+			name:                         "off by default",
+			controllerName:               "delta",
+			disabledByDefaultControllers: []string{"delta", "echo"},
+			controllers:                  []string{"*"}, // --controllers=*
+			expected:                     false,
 		},
 		{
-			name: "on by star, not off by name",
-			args: args{
-				controllerName:               "alpha",
-				disabledByDefaultControllers: []string{"delta", "echo"},
-			},
-			opts: Options{
-				Controllers: []string{"*", "-charlie"},
-			},
-			expected: true,
+			name:                         "on by star, not off by name",
+			controllerName:               "alpha",
+			disabledByDefaultControllers: []string{"delta", "echo"},
+			controllers:                  []string{"*", "-charlie"}, // --controllers=*,-charlie
+			expected:                     true,
 		},
 		{
-			name: "off by name with star",
-			args: args{
-				controllerName:               "charlie",
-				disabledByDefaultControllers: []string{"delta", "echo"},
-			},
-			opts: Options{
-				Controllers: []string{"*", "-charlie"},
-			},
-			expected: false,
+			name:                         "off by name with star",
+			controllerName:               "charlie",
+			disabledByDefaultControllers: []string{"delta", "echo"},
+			controllers:                  []string{"*", "-charlie"}, // --controllers=*,-charlie
+			expected:                     false,
 		},
 		{
-			name: "off by default implicit, no star",
-			args: args{
-				controllerName:               "foxtrot",
-				disabledByDefaultControllers: []string{"delta", "echo"},
-			},
-			opts: Options{
-				Controllers: []string{"alpha", "bravo", "-charlie"},
-			},
-			expected: false,
+			name:                         "off by default implicit, no star",
+			controllerName:               "foxtrot",
+			disabledByDefaultControllers: []string{"delta", "echo"},
+			controllers:                  []string{"alpha", "bravo", "-charlie"}, // --controllers=alpha,bravo,-charlie
+			expected:                     false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := Context{
-				Opts: tt.opts,
+				Opts: Options{
+					Controllers: tt.controllers,
+				},
 			}
-			if got := c.IsControllerEnabled(tt.args.controllerName, sets.NewString(tt.args.disabledByDefaultControllers...)); got != tt.expected {
-				t.Errorf("IsControllerEnabled() = %v, want %v", got, tt.expected)
+			if got := c.IsControllerEnabled(tt.controllerName, sets.NewString(tt.disabledByDefaultControllers...)); got != tt.expected {
+				t.Errorf("expected %v, but got %v", tt.expected, got)
 			}
 		})
 	}
