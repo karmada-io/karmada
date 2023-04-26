5b0f71004dcf77f4ab8aa23ba868db24d0d1d055
huangyanfeng <huangyanfeng1992@gmail.com>
2023-04-26 13:55:32 +0800

    Add kubebuilder shortName for work
    Signed-off-by: huangyanfeng <huangyanfeng1992@gmail.com>

--- pkg/apis/work/v1alpha1/work_types.go
+++ pkg/apis/work/v1alpha1/work_types.go
@@ -19,7 +19,7 @@ const (
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:subresource:status
-// +kubebuilder:resource:categories={karmada-io}
+// +kubebuilder:resource:categories={karmada-io},shortName=wk
 // +kubebuilder:printcolumn:JSONPath=`.status.conditions[?(@.type=="Applied")].status`,name="Applied",type=string
 // +kubebuilder:printcolumn:JSONPath=`.metadata.creationTimestamp`,name="Age",type=date
 
