fad05cea57f81ace84c9798b8ce4f5e7264bc316
changzhen <changzhen5@huawei.com>
2023-06-26 17:16:00 +0800

    helm: spilt karmada-config ConfigMap
    Signed-off-by: changzhen <changzhen5@huawei.com>

--- hack/update-crdgen.sh
+++ hack/update-crdgen.sh
@@ -13,10 +13,10 @@ echo "Generating with controller-gen"
 util::install_tools ${CONTROLLER_GEN_PKG} ${CONTROLLER_GEN_VER} >/dev/null 2>&1
 
 # Unify the crds used by helm chart and the installation scripts
-controller-gen crd paths=./pkg/apis/config/... output:crd:dir=./charts/karmada/_crds/bases
-controller-gen crd paths=./pkg/apis/policy/... output:crd:dir=./charts/karmada/_crds/bases
-controller-gen crd paths=./pkg/apis/work/... output:crd:dir=./charts/karmada/_crds/bases
-controller-gen crd paths=./pkg/apis/autoscaling/... output:crd:dir=./charts/karmada/_crds/bases
-controller-gen crd paths=./pkg/apis/networking/... output:crd:dir=./charts/karmada/_crds/bases
+controller-gen crd paths=./pkg/apis/config/... output:crd:dir=./charts/karmada/_crds/bases/config
+controller-gen crd paths=./pkg/apis/policy/... output:crd:dir=./charts/karmada/_crds/bases/policy
+controller-gen crd paths=./pkg/apis/work/... output:crd:dir=./charts/karmada/_crds/bases/work
+controller-gen crd paths=./pkg/apis/autoscaling/... output:crd:dir=./charts/karmada/_crds/bases/autoscaling
+controller-gen crd paths=./pkg/apis/networking/... output:crd:dir=./charts/karmada/_crds/bases/networking
 controller-gen crd paths=./examples/customresourceinterpreter/apis/... output:crd:dir=./examples/customresourceinterpreter/apis/
 controller-gen crd paths=./operator/pkg/apis/operator/... output:crd:dir=./charts/karmada-operator/crds
