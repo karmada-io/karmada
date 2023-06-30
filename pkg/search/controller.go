8fddeea550cabf08f7638bb3d1083ddd5d4eded1
lizhen6 <lizhen6@360.cn>
2023-06-30 09:50:41 +0800

    refactor search reconciling logic
    Signed-off-by: lizhen6 <lizhen6@360.cn>

--- pkg/search/controller.go
+++ pkg/search/controller.go
@@ -3,6 +3,7 @@ package search
 import (
 	"fmt"
 	"reflect"
+	"strings"
 	"sync"
 	"time"
 
@@ -11,6 +12,7 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -18,7 +20,7 @@ import (
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog/v2"
 
-	clusterV1alpha1 "github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1"
+	clusterv1alpha1 "github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1"
 	policyv1alpha1 "github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1"
 	searchv1alpha1 "github.com/karmada-io/karmada/pkg/apis/search/v1alpha1"
 	informerfactory "github.com/karmada-io/karmada/pkg/generated/informers/externalversions"
@@ -161,46 +163,209 @@ func (c *Controller) handleErr(err error, key interface{}) {
 	c.queue.AddRateLimited(key)
 }
 
-// doCacheCluster processes the resourceRegistry object
-// TODO: update status
-func (c *Controller) doCacheCluster(cluster string) error {
-	// STEP0:  stop informer manager for the cluster which is not referenced by any `SearchRegistry` object.
-	v, ok := c.clusterRegistry.Load(cluster)
-	if !ok {
-		klog.Infof("Cluster %s is not registered", cluster)
-		return nil
+func (c *Controller) getClusterMatchedRegistries(cluster *clusterv1alpha1.Cluster) (indexedByName, matched map[string]*searchv1alpha1.ResourceRegistry, err error) {
+	client := c.informerFactory.Search().V1alpha1().ResourceRegistries().Lister()
+	var registries []*searchv1alpha1.ResourceRegistry
+	if registries, err = client.List(labels.Everything()); err != nil {
+		klog.Errorf("List resource registries for reconciling cluster failed, err: %s", err)
+		return
 	}
-
-	cr := v.(clusterRegistry)
-	if cr.unregistry() {
-		klog.Infof("try to stop cluster informer %s", cluster)
-		c.InformerManager.Stop(cluster)
-		return nil
+	if len(registries) == 0 {
+		klog.Infof("No resource registries, no need to reconcile cluster")
+		return
 	}
+	indexedByName = make(map[string]*searchv1alpha1.ResourceRegistry, len(registries))
+	matched = make(map[string]*searchv1alpha1.ResourceRegistry, len(registries))
+	for i, registry := range registries {
+		indexedByName[registry.Name] = registries[i]
+		if util.ClusterMatches(cluster, registry.Spec.TargetCluster) {
+			matched[registry.Name] = registries[i]
+		}
+	}
+	return
+}
 
-	// STEP1: stop informer manager for the cluster which does not exist anymore or is not ready.
-	cls, err := c.clusterLister.Get(cluster)
+func (c *Controller) getRegistryAddedResources(registry *searchv1alpha1.ResourceRegistry, cr *clusterRegistry, added []schema.GroupVersionResource) []schema.GroupVersionResource {
+	resourcesToWatch := c.getResources(registry.Spec.ResourceSelectors)
+	for _, resource := range resourcesToWatch {
+		if resourcesOnWatching, exist := cr.resources[resource]; exist {
+			resourcesOnWatching[registry.Name] = struct{}{}
+		} else {
+			added = append(added, resource)
+			cr.resources[resource] = registrySet{registry.Name: struct{}{}}
+		}
+	}
+	return added
+}
+
+func (c *Controller) getClusterRegistriesModification(registries map[string]*searchv1alpha1.ResourceRegistry, matched map[string]*searchv1alpha1.ResourceRegistry, cr *clusterRegistry, added []string, removed []string) (updated *clusterRegistry, addedResources []schema.GroupVersionResource, removedResources []schema.GroupVersionResource) {
+	defer func() {
+		updated = cr
+	}()
+	if cr == nil {
+		cr = &clusterRegistry{
+			resources:  make(map[schema.GroupVersionResource]registrySet),
+			registries: make(registrySet)}
+	}
+	for _, name := range removed {
+		delete(cr.registries, name)
+		for resource, previousRegistries := range cr.resources {
+			delete(previousRegistries, name)
+			if len(previousRegistries) == 0 {
+				removedResources = append(removedResources, resource)
+			}
+		}
+		for _, resource := range removedResources {
+			delete(cr.resources, resource)
+		}
+	}
+	for _, name := range added {
+		cr.registries[name] = struct{}{}
+		addedResources = c.getRegistryAddedResources(registries[name], cr, addedResources)
+	}
+	for _, registry := range matched {
+		addedResources = c.getRegistryAddedResources(registry, cr, addedResources)
+	}
+	return
+}
+
+func (c *Controller) findOpenSearchBackend(matchedRegistries map[string]*searchv1alpha1.ResourceRegistry) *searchv1alpha1.BackendStoreConfig {
+	for _, registry := range matchedRegistries {
+		if backend := registry.Spec.BackendStore; backend != nil && backend.OpenSearch != nil {
+			// one cluster may be related to multi registries,
+			// however only one backend could keep in memory with one cluster
+			return backend
+		}
+	}
+	return nil
+}
+
+func (c *Controller) clusterAbleToCache(cluster string) (cls *clusterv1alpha1.Cluster, able bool, err error) {
+	cls, err = c.clusterLister.Get(cluster)
 	if err != nil {
 		if apierrors.IsNotFound(err) {
-			klog.Infof("try to stop cluster informer %s", cluster)
+			klog.Infof("Try to stop cluster informer %s", cluster)
 			c.InformerManager.Stop(cluster)
-			return nil
+			return
 		}
-		return err
+		return
 	}
 
 	if !cls.DeletionTimestamp.IsZero() {
-		klog.Infof("try to stop cluster informer %s", cluster)
+		klog.Infof("Try to stop cluster informer %s", cluster)
 		c.InformerManager.Stop(cluster)
-		return nil
+		return
 	}
 
 	if !util.IsClusterReady(&cls.Status) {
 		klog.Warningf("cluster %s is notReady try to stop this cluster informer", cluster)
 		c.InformerManager.Stop(cluster)
+		return
+	}
+	able = true
+	return
+}
+
+func (c *Controller) reconcileClusterWithRegistries(cls *clusterv1alpha1.Cluster) (matchedRegistries map[string]*searchv1alpha1.ResourceRegistry, cr *clusterRegistry, resourcesChanged, newRegistry bool, err error) {
+	cluster := cls.Name
+	var allRegistries map[string]*searchv1alpha1.ResourceRegistry
+	allRegistries, matchedRegistries, err = c.getClusterMatchedRegistries(cls)
+	if err != nil {
+		klog.Errorf("Get cluster matched resource registries failed, cluster: %s, error: %s", cluster, err)
+		return
+	}
+	klog.V(4).Infof("Registries matched with cluster, cluster: %s, registries: %s",
+		cluster, strings.Join(util.Keys(matchedRegistries), ", "))
+	v, hasExistingRegistries := c.clusterRegistry.Load(cluster)
+	var addedRegistries, removedRegistries []string
+	if !hasExistingRegistries {
+		klog.Infof("Cluster %s has no existing registries", cluster)
+		addedRegistries, removedRegistries = util.DiffKey(registrySet(nil), matchedRegistries)
+	} else {
+		crv := v.(clusterRegistry)
+		cr = &crv
+		addedRegistries, removedRegistries = util.DiffKey(cr.registries, matchedRegistries)
+	}
+	if len(addedRegistries) > 0 {
+		klog.V(4).Infof("New registries added to cluster, cluster: %s, registries: %s", cluster, strings.Join(addedRegistries, ", "))
+	} else {
+		klog.V(4).Infof("No registries added to cluster, cluster: %s", cluster)
+	}
+	if len(removedRegistries) > 0 {
+		klog.V(4).Infof("Old registries removed from cluster, cluster: %s, registries: %s", cluster, strings.Join(removedRegistries, ", "))
+	} else {
+		klog.V(4).Infof("No registries removed from cluster, cluster: %s", cluster)
+	}
+	var addedResources, removedResources []schema.GroupVersionResource
+	cr, addedResources, removedResources = c.getClusterRegistriesModification(allRegistries, matchedRegistries, cr, addedRegistries, removedRegistries)
+	currentWatchingResources := util.Keys(cr.resources)
+	resourcesString := util.StringerJoin(currentWatchingResources, ", ")
+	if len(addedResources) > 0 {
+		klog.V(4).Infof("New watching resources added to cluster, cluster: %s, resources: %s", cluster, util.StringerJoin(addedResources, ", "))
+	} else {
+		klog.V(4).Infof("No watching resources added to cluster, cluster: %s, resources: %s", cluster, resourcesString)
+	}
+	if len(removedResources) > 0 {
+		klog.V(4).Infof("Resources watching removed from cluster, cluster: %s, resources: %s", cluster, util.StringerJoin(removedResources, ", "))
+	} else {
+		klog.V(4).Infof("No resources watching removed from cluster, cluster: %s, resources: %s", cluster, resourcesString)
+	}
+	resourcesChanged = len(addedResources) > 0 || len(removedResources) > 0
+	newRegistry = !hasExistingRegistries
+	return
+}
+
+func (c *Controller) getRegistryBackendHandler(cluster string, matchedRegistries map[string]*searchv1alpha1.ResourceRegistry) (cache.ResourceEventHandler, error) {
+	backend := backendstore.GetBackend(cluster)
+	if backend == nil {
+		backendConfig := c.findOpenSearchBackend(matchedRegistries)
+		backendstore.AddBackend(cluster, backendConfig)
+		backend = backendstore.GetBackend(cluster)
+	}
+	if backend == nil {
+		return nil, fmt.Errorf("failed to get backend store for cluster %s", cluster)
+	}
+	handler := backend.ResourceEventHandlerFuncs()
+	if handler == nil {
+		return nil, fmt.Errorf("failed to get resource event handler for cluster %s", cluster)
+	}
+	return handler, nil
+}
+
+// doCacheCluster processes the resourceRegistry object
+// TODO: update status
+func (c *Controller) doCacheCluster(cluster string) error {
+	// STEP0: stop informer manager for the cluster which does not exist anymore or is not ready.
+	cls, able, err := c.clusterAbleToCache(cluster)
+	if err != nil || !able {
+		return err
+	}
+	matchedRegistries, cr, resourcesChanged, newRegistry, err := c.reconcileClusterWithRegistries(cls)
+	if err != nil {
+		return err
+	}
+
+	// STEP1:  stop informer manager for the cluster which is not referenced by any `SearchRegistry` object.
+	if cr.unregistry() {
+		klog.Infof("Try to stop cluster informer %s", cluster)
+		c.InformerManager.Stop(cluster)
 		return nil
 	}
 
+	if resourcesChanged {
+		klog.Infof("Create new informer for cluster %s", cluster)
+		c.InformerManager.Stop(cluster)
+	}
+
+	if newRegistry {
+		c.clusterRegistry.Store(cluster, *cr)
+	}
+	handler, err := c.getRegistryBackendHandler(cluster, matchedRegistries)
+	if err != nil {
+		return err
+	}
+
+	var newInformerCreated bool
 	// STEP2: added/updated cluster, builds an informer manager for a specific cluster.
 	if !c.InformerManager.IsManagerExist(cluster) {
 		klog.Info("Try to build informer manager for cluster ", cluster)
@@ -211,27 +376,23 @@ func (c *Controller) doCacheCluster(cluster string) error {
 			return err
 		}
 		_ = c.InformerManager.ForCluster(cluster, clusterDynamicClient.DynamicClientSet, 0)
+		newInformerCreated = true
 	}
 
-	handler := backendstore.GetBackend(cluster).ResourceEventHandlerFuncs()
-	if handler == nil {
-		return fmt.Errorf("failed to get resource event handler for cluster %s", cluster)
+	if !newInformerCreated && !resourcesChanged {
+		return nil
 	}
 
 	sci := c.InformerManager.GetSingleClusterManager(cluster)
-	for gvr, registries := range cr.resources {
-		if len(registries) == 0 {
-			// unregisted resource, do nothing.
-			continue
-		}
-
-		klog.Infof("add informer for %s, %v", cluster, gvr)
+	for gvr := range cr.resources {
+		klog.Infof("Add informer for %s, %v", cluster, gvr)
 		sci.ForResource(gvr, handler)
 	}
-	klog.Infof("start informer for %s", cluster)
+
+	klog.Infof("Start informer for %s", cluster)
 	sci.Start()
 	_ = sci.WaitForCacheSync()
-	klog.Infof("start informer for %s done", cluster)
+	klog.Infof("Start informer for %s done", cluster)
 
 	return nil
 }
@@ -239,28 +400,8 @@ func (c *Controller) doCacheCluster(cluster string) error {
 // addResourceRegistry parse the resourceRegistry object and add Cluster to the queue
 func (c *Controller) addResourceRegistry(obj interface{}) {
 	rr := obj.(*searchv1alpha1.ResourceRegistry)
-	resources := c.getResources(rr.Spec.ResourceSelectors)
 
 	for _, cluster := range c.getClusters(rr.Spec.TargetCluster) {
-		v, _ := c.clusterRegistry.LoadOrStore(cluster, clusterRegistry{
-			resources:  make(map[schema.GroupVersionResource]registrySet),
-			registries: make(registrySet)})
-		cr := v.(clusterRegistry)
-
-		for _, r := range resources {
-			if cr.resources[r] == nil {
-				cr.resources[r] = make(registrySet)
-			}
-			// add registry record for the resource.
-			cr.resources[r][rr.GetName()] = struct{}{}
-		}
-		// add registry record for the cluster.
-		cr.registries[rr.GetName()] = struct{}{}
-		c.clusterRegistry.Store(cluster, cr)
-
-		// set backendstore
-		backendstore.AddBackend(cluster, rr.Spec.BackendStore)
-
 		c.queue.Add(cluster)
 	}
 }
@@ -274,58 +415,13 @@ func (c *Controller) updateResourceRegistry(oldObj, newObj interface{}) {
 		klog.V(4).Infof("Ignore ResourceRegistry(%s) update event as spec not changed", newRR.Name)
 		return
 	}
-
-	// TODO: stop resource informers if it is not in the new resource registry
-	resources := c.getResources(newRR.Spec.ResourceSelectors)
-
-	clusters := c.getClusters(newRR.Spec.TargetCluster)
-	clusterSets := make(map[string]struct{}, len(clusters))
-
-	for _, cls := range clusters {
-		v, _ := c.clusterRegistry.LoadOrStore(cls, clusterRegistry{
-			resources:  make(map[schema.GroupVersionResource]registrySet),
-			registries: make(registrySet)})
-		cr := v.(clusterRegistry)
-
-		for _, r := range resources {
-			if cr.resources[r] == nil {
-				cr.resources[r] = make(registrySet)
-			}
-			// add registry record for the resource.
-			cr.resources[r][newRR.GetName()] = struct{}{}
-		}
-		// add registry record for the cluster.
-		cr.registries[newRR.GetName()] = struct{}{}
-		c.clusterRegistry.Store(cls, cr)
-
-		clusterSets[cls] = struct{}{}
-
-		// set backendstore
-		backendstore.AddBackend(cls, newRR.Spec.BackendStore)
-
-		c.queue.Add(cls)
+	clusterSet := sets.New[string]()
+	for _, registry := range []*searchv1alpha1.ResourceRegistry{newRR, oldRR} {
+		clusters := c.getClusters(registry.Spec.TargetCluster)
+		clusterSet.Insert(clusters...)
 	}
-
-	for _, cls := range c.getClusters(oldRR.Spec.TargetCluster) {
-		if _, ok := clusterSets[cls]; ok {
-			// Cluster is in both old and new clusters, do nothing.
-			continue
-		}
-
-		v, ok := c.clusterRegistry.Load(cls)
-		if !ok {
-			// unregisted cluster, do nothing.
-			continue
-		}
-
-		cr := v.(clusterRegistry)
-		delete(cr.registries, oldRR.GetName())
-		if cr.unregistry() {
-			cr.resources = make(map[schema.GroupVersionResource]registrySet)
-		}
-		c.clusterRegistry.Store(cls, cr)
-
-		c.queue.Add(cls)
+	for _, cluster := range clusterSet.UnsortedList() {
+		c.queue.Add(cluster)
 	}
 }
 
@@ -348,51 +444,22 @@ func (c *Controller) deleteResourceRegistry(obj interface{}) {
 	}
 
 	for _, cluster := range c.getClusters(rr.Spec.TargetCluster) {
-		v, ok := c.clusterRegistry.Load(cluster)
-		if !ok {
-			// unregisted cluster, do nothing.
-			continue
-		}
-
-		cr := v.(clusterRegistry)
-		// delete registry record for the cluster.
-		delete(cr.registries, rr.GetName())
-		if cr.unregistry() {
-			cr.resources = make(map[schema.GroupVersionResource]registrySet)
-		}
-
-		// delete registry record for the resource.
-		for k := range cr.resources {
-			delete(cr.resources[k], rr.GetName())
-		}
-
-		c.clusterRegistry.Store(cluster, cr)
-
 		c.queue.Add(cluster)
 	}
 }
 
 // addCluster adds a cluster object to the queue if needed
 func (c *Controller) addCluster(obj interface{}) {
-	cluster := obj.(*clusterV1alpha1.Cluster)
-	_, ok := c.clusterRegistry.Load(cluster.GetName())
-	if ok {
-		// unregistered cluster, do nothing.
-		return
-	}
+	cluster := obj.(*clusterv1alpha1.Cluster)
+
 	c.queue.Add(cluster.GetName())
 }
 
 // updateCluster rebuild informer if Cluster.Spec is changed
 func (c *Controller) updateCluster(oldObj, curObj interface{}) {
-	curCluster := curObj.(*clusterV1alpha1.Cluster)
-	_, ok := c.clusterRegistry.Load(curCluster.GetName())
-	if !ok {
-		// unregistered cluster, do nothing.
-		return
-	}
+	curCluster := curObj.(*clusterv1alpha1.Cluster)
 
-	oldCluster := oldObj.(*clusterV1alpha1.Cluster)
+	oldCluster := oldObj.(*clusterv1alpha1.Cluster)
 	if curCluster.ResourceVersion == oldCluster.ResourceVersion {
 		// no change, do nothing.
 		return
@@ -403,16 +470,14 @@ func (c *Controller) updateCluster(oldObj, curObj interface{}) {
 		c.queue.Add(curCluster.GetName())
 	}
 
-	if !reflect.DeepEqual(curCluster.Spec, oldCluster.Spec) {
-		// Cluster.Spec is changed, rebuild informer.
-		c.InformerManager.Stop(curCluster.GetName())
+	if !reflect.DeepEqual(curCluster.Spec, oldCluster.Spec) || !reflect.DeepEqual(curCluster.Labels, oldCluster.Labels) {
 		c.queue.Add(curCluster.GetName())
 	}
 }
 
 // deleteCluster set cluster to not exists
 func (c *Controller) deleteCluster(obj interface{}) {
-	cluster, isCluster := obj.(*clusterV1alpha1.Cluster)
+	cluster, isCluster := obj.(*clusterv1alpha1.Cluster)
 	// We can get DeletedFinalStateUnknown instead of *clusterV1alpha1.Cluster here and
 	// we need to handle that correctly.
 	if !isCluster {
@@ -421,7 +486,7 @@ func (c *Controller) deleteCluster(obj interface{}) {
 			klog.ErrorS(nil, "Received unexpected object", "object", obj)
 			return
 		}
-		cluster, ok = deletedState.Obj.(*clusterV1alpha1.Cluster)
+		cluster, ok = deletedState.Obj.(*clusterv1alpha1.Cluster)
 		if !ok {
 			klog.ErrorS(nil, "DeletedFinalStateUnknown contained non-Cluster object", "object", deletedState.Obj)
 			return
