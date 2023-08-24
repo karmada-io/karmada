824bf77784f735b4a5cb8ad91e1114f930a95a7c
changzhen <changzhen5@huawei.com>
2023-08-24 13:38:27 +0800

    unBoxing EndpointSlice with specified LabelServiceName Label according MultiClusterService object
    Signed-off-by: changzhen <changzhen5@huawei.com>

--- pkg/controllers/mcs/endpointslice_controller.go
+++ pkg/controllers/mcs/endpointslice_controller.go
@@ -5,9 +5,9 @@ import (
 
 	discoveryv1 "k8s.io/api/discovery/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/klog/v2"
 	controllerruntime "sigs.k8s.io/controller-runtime"
@@ -16,6 +16,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
+	networkingv1alpha1 "github.com/karmada-io/karmada/pkg/apis/networking/v1alpha1"
 	workv1alpha1 "github.com/karmada-io/karmada/pkg/apis/work/v1alpha1"
 	"github.com/karmada-io/karmada/pkg/util"
 	"github.com/karmada-io/karmada/pkg/util/helper"
@@ -25,7 +26,14 @@ import (
 // EndpointSliceControllerName is the controller name that will be used when reporting events.
 const EndpointSliceControllerName = "endpointslice-controller"
 
-// EndpointSliceController is to collect EndpointSlice which reported by member cluster from executionNamespace to serviceexport namespace.
+const (
+	mcsDiscoveryAnnotationKey = "discovery.karmada.io/strategy"
+
+	mcsDiscoveryRemoteAndLocalStrategy = "RemoteAndLocal"
+)
+
+// EndpointSliceController is to collect EndpointSlice which reported by member cluster
+// from executionNamespace to serviceexport namespace.
 type EndpointSliceController struct {
 	client.Client
 	EventRecorder record.EventRecorder
@@ -55,7 +63,7 @@ func (c *EndpointSliceController) Reconcile(ctx context.Context, req controllerr
 		return controllerruntime.Result{}, nil
 	}
 
-	return c.collectEndpointSliceFromWork(work)
+	return controllerruntime.Result{}, c.unBoxingEndpointSlice(work)
 }
 
 // SetupWithManager creates a controller and register to controller manager.
@@ -77,49 +85,59 @@ func (c *EndpointSliceController) SetupWithManager(mgr controllerruntime.Manager
 	return controllerruntime.NewControllerManagedBy(mgr).For(&workv1alpha1.Work{}, builder.WithPredicates(serviceImportPredicateFun)).Complete(c)
 }
 
-func (c *EndpointSliceController) collectEndpointSliceFromWork(work *workv1alpha1.Work) (controllerruntime.Result, error) {
+func (c *EndpointSliceController) unBoxingEndpointSlice(work *workv1alpha1.Work) error {
 	clusterName, err := names.GetClusterName(work.Namespace)
 	if err != nil {
 		klog.Errorf("Failed to get cluster name for work %s/%s", work.Namespace, work.Name)
-		return controllerruntime.Result{Requeue: true}, err
+		return err
 	}
 
 	for _, manifest := range work.Spec.Workload.Manifests {
 		unstructObj := &unstructured.Unstructured{}
-		if err := unstructObj.UnmarshalJSON(manifest.Raw); err != nil {
+		if err = unstructObj.UnmarshalJSON(manifest.Raw); err != nil {
 			klog.Errorf("Failed to unmarshal workload, error is: %v", err)
-			return controllerruntime.Result{Requeue: true}, err
+			return err
 		}
 
 		endpointSlice := &discoveryv1.EndpointSlice{}
 		err = helper.ConvertToTypedObject(unstructObj, endpointSlice)
 		if err != nil {
 			klog.Errorf("Failed to convert unstructured to typed object: %v", err)
-			return controllerruntime.Result{Requeue: true}, err
+			return err
 		}
 
-		desiredEndpointSlice := deriveEndpointSlice(endpointSlice, clusterName)
-		desiredEndpointSlice.Labels = map[string]string{
-			workv1alpha1.WorkNamespaceLabel: work.Namespace,
-			workv1alpha1.WorkNameLabel:      work.Name,
-			discoveryv1.LabelServiceName:    names.GenerateDerivedServiceName(work.Labels[util.ServiceNameLabel]),
-			util.ManagedByKarmadaLabel:      util.ManagedByKarmadaLabelValue,
+		mcsNamespacedName := types.NamespacedName{
+			Namespace: endpointSlice.Namespace,
+			Name:      work.Labels[util.ServiceNameLabel],
+		}
+		mcs := &networkingv1alpha1.MultiClusterService{}
+		err = c.Get(context.TODO(), mcsNamespacedName, mcs)
+		if err != nil {
+			klog.Errorf("Failed to get ref MultiClusterService(%s): %v", mcsNamespacedName.String(), err)
+			return err
 		}
 
+		desiredEndpointSlice := deriveEndpointSlice(endpointSlice, clusterName, mcs)
 		if err = helper.CreateOrUpdateEndpointSlice(c.Client, desiredEndpointSlice); err != nil {
-			return controllerruntime.Result{Requeue: true}, err
+			return err
 		}
 	}
 
-	return controllerruntime.Result{}, nil
+	return nil
 }
 
-func deriveEndpointSlice(original *discoveryv1.EndpointSlice, migratedFrom string) *discoveryv1.EndpointSlice {
-	endpointSlice := original.DeepCopy()
-	endpointSlice.ObjectMeta = metav1.ObjectMeta{
-		Namespace: original.Namespace,
-		Name:      names.GenerateEndpointSliceName(original.GetName(), migratedFrom),
+func deriveEndpointSlice(eps *discoveryv1.EndpointSlice, cluster string, mcs *networkingv1alpha1.MultiClusterService) *discoveryv1.EndpointSlice {
+	eps.Name = names.GenerateEndpointSliceName(eps.Name, cluster)
+	eps.Labels = map[string]string{
+		discoveryv1.LabelServiceName: labelServiceName(mcs),
 	}
+	return eps
+}
 
-	return endpointSlice
+func labelServiceName(mcs *networkingv1alpha1.MultiClusterService) string {
+	strategy, exist := mcs.Annotations[mcsDiscoveryAnnotationKey]
+	if exist && strategy == mcsDiscoveryRemoteAndLocalStrategy {
+		return mcs.Name
+	}
+	return names.GenerateDerivedServiceName(mcs.Name)
 }
