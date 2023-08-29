751e8266023ef2206866e5352c54ff7d33344f9f
Lan Liang <gcslyp@gmail.com>
2023-08-29 08:22:03 +0000

    Move all of images component to images.go
    Signed-off-by: Lan Liang <gcslyp@gmail.com>

---
+++ pkg/karmadactl/util/constants.go
@@ -0,0 +1,22 @@
+package util
+
+const (
+	// Etcd defines variable used internally when referring to etcd component
+	Etcd = "etcd"
+	// KubeAPIServer defines variable used internally when referring to kube-apiserver component
+	KubeAPIServer = "kube-apiserver"
+	// KubeControllerManager defines variable used internally when referring to kube-controller-manager component
+	KubeControllerManager = "kube-controller-manager"
+	// KarmadaScheduler defines variable used internally when referring to karmada-scheduler component
+	KarmadaScheduler = "karmada-scheduler"
+	// KarmadaControllerManager defines variable used internally when referring to the karmada-controller-manager
+	KarmadaControllerManager = "karmada-controller-manager"
+	// KarmadaAggregatedApiserver defines variable used internally when referring to the karmada-aggregated-apiserver
+	KarmadaAggregatedAPIServer = "karmada-aggregated-apiserver"
+	// KarmadaWebhook defines variable used internally when referring to the karmada-webhook
+	KarmadaWebhook = "karmada-webhook"
+	// alpine defines variable used internally when referring to the alpine
+	Alpine = "alpine"
+	// DockerIO defines variable used internally when referring to the docker.io
+	DockerIO = "docker.io"
+)
