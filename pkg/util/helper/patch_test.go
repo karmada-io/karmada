85202278971a02f09579aedd4c3b67a203df61b0
zach593 <zach_li@outlook.com>
2023-09-30 22:12:51 +0800

    improve update workload status error rate too high
    Change-Id: I531e40fa99e657af3c060e59c13c47a835356274
    Signed-off-by: zach593 <zach_li@outlook.com>

--- pkg/util/helper/patch_test.go
+++ pkg/util/helper/patch_test.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"math"
 	"testing"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -102,6 +103,160 @@ func TestGenMergePatch(t *testing.T) {
 			}
 			if string(patch) != tt.expectedPatch {
 				t.Fatalf("want patch: %s, but got :%s", tt.expectedPatch, string(patch))
+			}
+		})
+	}
+}
+
+func TestGenJSONPatch(t *testing.T) {
+	//doc := `{"field1":1,"field2":"2","field3":[3],"field4":{"a":"a"}}`
+	type args struct {
+		op    string
+		from  string
+		path  string
+		value interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "add object field",
+			args: args{
+				op:    "add",
+				path:  "/abc",
+				value: 1,
+			},
+			want:    `[{"op":"add","path":"/abc","value":1}]`,
+			wantErr: false,
+		},
+		{
+			name: "replace object field",
+			args: args{
+				op:    "replace",
+				path:  "/abc",
+				value: 1,
+			},
+			want:    `[{"op":"replace","path":"/abc","value":1}]`,
+			wantErr: false,
+		},
+		{
+			name: "remove object field, redundant args will be ignored",
+			args: args{
+				op:    "remove",
+				from:  "123",
+				path:  "/abc",
+				value: 1,
+			},
+			want:    `[{"op":"remove","path":"/abc"}]`,
+			wantErr: false,
+		},
+		{
+			name: "move object field",
+			args: args{
+				op:   "move",
+				from: "/abc",
+				path: "/123",
+			},
+			want:    `[{"op":"move","from":"/abc","path":"/123"}]`,
+			wantErr: false,
+		},
+		{
+			name: "copy object field, redundant array value will be ignored",
+			args: args{
+				op:    "copy",
+				from:  "/123",
+				path:  "/abc",
+				value: []interface{}{1, "a", false, 4.5},
+			},
+			want:    `[{"op":"copy","from":"/123","path":"/abc"}]`,
+			wantErr: false,
+		},
+		{
+			name: "replace object field, input string typed number",
+			args: args{
+				op:    "replace",
+				path:  "/abc",
+				value: "1",
+			},
+			want:    `[{"op":"replace","path":"/abc","value":"1"}]`,
+			wantErr: false,
+		},
+		{
+			name: "replace object field, input invalid type",
+			args: args{
+				op:    "replace",
+				path:  "/abc",
+				value: make(chan int),
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "replace object field, input invalid value",
+			args: args{
+				op:    "replace",
+				path:  "/abc",
+				value: math.Inf(1),
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "replace object field, input struct value",
+			args: args{
+				op:   "replace",
+				path: "/abc",
+				value: struct {
+					A string
+					B int
+					C float64
+					D bool
+				}{"a", 1, 1.2, true},
+			},
+			want:    `[{"op":"replace","path":"/abc","value":{"A":"a","B":1,"C":1.2,"D":true}}]`,
+			wantErr: false,
+		},
+		{
+			name: "test object field, input array value",
+			args: args{
+				op:    "test",
+				path:  "/abc",
+				value: []interface{}{1, "a", false, 4.5},
+			},
+			want:    `[{"op":"test","path":"/abc","value":[1,"a",false,4.5]}]`,
+			wantErr: false,
+		},
+		{
+			name: "move object field, input invalid path, but we won't verify it",
+			args: args{
+				op:   "move",
+				from: "123",
+				path: "abc",
+			},
+			want:    `[{"op":"move","from":"123","path":"abc"}]`,
+			wantErr: false,
+		},
+		{
+			name: "input invalid op",
+			args: args{
+				op:   "whatever",
+				path: "/abc",
+			},
+			want:    "",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			patchBytes, err := GenJSONPatch(tt.args.op, tt.args.from, tt.args.path, tt.args.value)
+			if tt.wantErr != (err != nil) {
+				t.Errorf("wantErr: %v, but got err: %v", tt.wantErr, err)
+			}
+			if tt.want != string(patchBytes) {
+				t.Errorf("want: %s, but got: %s", tt.want, patchBytes)
 			}
 		})
 	}
