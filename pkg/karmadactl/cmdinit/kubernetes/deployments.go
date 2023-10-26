9c651a2ea194883961d7a7350ae8fc613e7c4c40
huangyanfeng <huangyanfeng1992@gmail.com>
2023-10-26 14:31:28 +0800

    set MinVersion to VersionTLS13 for tlsconfig in karmada-apiserver and karmada-aggregated-apiserver
    Signed-off-by: huangyanfeng <huangyanfeng1992@gmail.com>

--- pkg/karmadactl/cmdinit/kubernetes/deployments.go
+++ pkg/karmadactl/cmdinit/kubernetes/deployments.go
@@ -90,7 +90,7 @@ func (i *CommandInitOption) karmadaAPIServerContainerCommand() []string {
 		"--requestheader-username-headers=X-Remote-User",
 		fmt.Sprintf("--tls-cert-file=%s/%s.crt", karmadaCertsVolumeMountPath, options.ApiserverCertAndKeyName),
 		fmt.Sprintf("--tls-private-key-file=%s/%s.key", karmadaCertsVolumeMountPath, options.ApiserverCertAndKeyName),
-	}
+		"--tls-min-version=VersionTLS13"}
 }
 
 func (i *CommandInitOption) makeKarmadaAPIServerDeployment() *appsv1.Deployment {
@@ -805,6 +805,7 @@ func (i *CommandInitOption) makeKarmadaAggregatedAPIServerDeployment() *appsv1.D
 					fmt.Sprintf("--etcd-keyfile=%s/%s.key", karmadaCertsVolumeMountPath, options.EtcdClientCertAndKeyName),
 					fmt.Sprintf("--tls-cert-file=%s/%s.crt", karmadaCertsVolumeMountPath, options.KarmadaCertAndKeyName),
 					fmt.Sprintf("--tls-private-key-file=%s/%s.key", karmadaCertsVolumeMountPath, options.KarmadaCertAndKeyName),
+					"--tls-min-version=VersionTLS13",
 					"--audit-log-path=-",
 					"--feature-gates=APIPriorityAndFairness=false",
 					"--audit-log-maxage=0",
