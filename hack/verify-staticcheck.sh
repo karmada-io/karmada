5f40e2369aaf7181d083591a2b43e5a718e11732
zhzhuang-zju <m17799853869@163.com>
2024-02-17 11:43:25 +0800

    provide ways to fix golangci-lint error
    Signed-off-by: zhzhuang-zju <m17799853869@163.com>

--- hack/verify-staticcheck.sh
+++ hack/verify-staticcheck.sh
@@ -38,6 +38,7 @@ if golangci-lint run; then
 else
   echo # print one empty line, separate from warning messages.
   echo 'Please review the above warnings.'
+  echo 'If the above warnings make sense, run "golangci-lint run --fix" to auto fix it'
   echo 'If the above warnings do not make sense, feel free to file an issue.'
   exit 1
 fi
