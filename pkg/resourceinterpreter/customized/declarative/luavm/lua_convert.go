e2babc3d24733fa6d8f3142a018be44ca29e3a60
chaosi-zju <chaosi@zju.edu.cn>
2024-03-04 12:41:48 +0800

    fix: avoid delete the key with empty value in object (lua table)
    Signed-off-by: chaosi-zju <chaosi@zju.edu.cn>

--- pkg/resourceinterpreter/customized/declarative/luavm/lua_convert.go
+++ pkg/resourceinterpreter/customized/declarative/luavm/lua_convert.go
@@ -17,90 +17,223 @@ limitations under the License.
 package luavm
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"reflect"
+	"strings"
 
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
 	lua "github.com/yuin/gopher-lua"
 	"k8s.io/apimachinery/pkg/conversion"
+	"k8s.io/apimachinery/pkg/util/sets"
 	luajson "layeh.com/gopher-json"
 )
 
 // ConvertLuaResultInto convert lua result to obj
-func ConvertLuaResultInto(luaResult lua.LValue, obj interface{}) error {
-	t, err := conversion.EnforcePtr(obj)
+// @param references: the method of encoding a lua table is flawed and needs to be referenced by the same Kind object.
+// take `Retain(desired, observed)` for example, we have problem to convert empty fields, but empty fields is most likely
+// from desired or observed object, so take {desired, observed} object as references to obtain more accurate encoding values.
+func ConvertLuaResultInto(luaResult *lua.LTable, obj interface{}, references ...any) error {
+	objReflect, err := conversion.EnforcePtr(obj)
 	if err != nil {
 		return fmt.Errorf("obj is not pointer")
 	}
 
-	// For example, `GetReplicas` returns requirement with empty:
-	//     {
-	//         nodeClaim: {},
-	//         resourceRequest: {
-	//             cpu: "100m"
-	//         }
-	//     }
-	// Luajson encodes it to
-	//     {"nodeClaim": [], "resourceRequest": {"cpu": "100m"}}
-	//
-	// While go json fails to unmarshal `[]` to ReplicaRequirements.NodeClaim object.
-	// ReplicaRequirements object.
-	//
-	// Here we handle it as follows:
-	//   1. Walk the object (lua table), delete the key with empty value (`nodeClaim` in this example):
-	//     {
-	//         resourceRequest: {
-	//             cpu: "100m"
-	//         }
-	//     }
-	//   2. Encode the object with luajson to be:
-	//     {"resourceRequest": {"cpu": "100m"}}
-	//   4. Finally, unmarshal the new json to object, get
-	//     {
-	//         resourceRequest: {
-	//             cpu: "100m"
-	//         }
-	//     }
-	isEmptyDic := func(v *lua.LTable) bool {
-		count := 0
-		v.ForEach(func(lua.LValue, lua.LValue) {
-			count++
-		})
-		return count == 0
-	}
-
-	var walkValue func(v lua.LValue)
-	walkValue = func(v lua.LValue) {
-		if t, ok := v.(*lua.LTable); ok {
-			t.ForEach(func(key lua.LValue, value lua.LValue) {
-				if tt, ok := value.(*lua.LTable); ok {
-					if isEmptyDic(tt) {
-						// set nil to delete key
-						t.RawSetH(key, lua.LNil)
-					} else {
-						walkValue(value)
-					}
-				}
-			})
-		}
-	}
-	walkValue(luaResult)
-
+	// 1. convert lua.LTable to json bytes
 	jsonBytes, err := luajson.Encode(luaResult)
 	if err != nil {
-		return fmt.Errorf("json Encode obj eroor %v", err)
+		return fmt.Errorf("encode lua result to json failed: %+v", err)
 	}
 
-	//  for lua an empty object by json encode be [] not {}
-	if t.Kind() == reflect.Struct && len(jsonBytes) > 1 && jsonBytes[0] == '[' {
-		jsonBytes[0], jsonBytes[len(jsonBytes)-1] = '{', '}'
+	// 2. the json bytes above step got may not be expected in some special case, and should be adjusted manually.
+	// because as for lua.LTable type, if a field is non-empty struct, it can be correctly encoded into struct format json;
+	// but if a field is empty struct, it will be encoded into empty slice format as '[]' (root cause is empty lua.LTable
+	// can not be distinguished from empty slice or empty struct).
+	//
+	// Supposing an object contains empty fileds, like following one has an empty slice field and an empty struct field.
+	// e.g: struct{one-filed: {}, another-field: []}
+	//
+	// When it is converted to lua.LTable, empty slice and empty struct are all converted to lua.LTable{}, which can't be distinguished.
+	// e.g: lua.LTable{one-filed: lua.LTable{}, another-field: lua.LTable{}}
+	//
+	// When the lua.LTable is encoded into json, the two empty field are all encoded into empty slice format as '[]'.
+	// e.g: {one-filed: [], another-field: []}
+	//
+	// Actually, this json value is not the expected, so we need to do some extra processing.
+	// we should convert `one-filed` back to {}, and keep `another-field` unchanged.
+	// expected: {one-filed: {}, another-field: []}
+	isStruct := objReflect.Kind() == reflect.Struct
+	jsonBytes, err = convertEmptySliceToEmptyStructPartly(jsonBytes, isStruct, references)
+	if err != nil {
+		return fmt.Errorf("convert empty object back to empty slice failed: %+v", err)
 	}
 
+	// 3. convert the converted json bytes to target object
 	err = json.Unmarshal(jsonBytes, obj)
 	if err != nil {
-		return fmt.Errorf("can not unmarshal %v to %#v：%v", string(jsonBytes), obj, err)
+		return fmt.Errorf("json unmarshal obj failed: %+v", err)
 	}
+
 	return nil
+}
+
+// convertEmptySliceToEmptyStructPartly convert certain json fields with `[]` value which originally is empty struct back to `{}`.
+// However, given a json bytes like {one-filed:[],another-field:[]}, the way to judge `one-filed:[]` should convert to `one-filed:{}`,
+// while `another-field:[]` should keep unchanged can be described as follows:
+//
+// Actually, empty struct/slice value are not generated out of thin air, take `Retain(desired, observed)` for example,
+// empty struct/slice value may come from three sources:
+//  1. originally from desired object
+//  2. originally from observed object
+//  3. user added in Custom ResourceInterpreter (the probability is low and not recommended)
+//
+// For the first two cases, we just need to iterate through the json bytes of `desired` and `observed` objects (called as references),
+// and record the empty slice fields in map `fieldOfEmptySlice`, record the empty struct fields in map `fieldOfEmptyStruct`,
+// finally iterate through the given json bytes:
+//  1. if an empty field ever existed in `fieldOfEmptySlice`, then it originally is slice, should keep unchanged.
+//  2. if an empty field ever existed in `fieldOfEmptyStruct`, then it originally is struct, should be change back to struct.
+//  3. if an empty field not ever existed in either map, is user added in Custom ResourceInterpreter,
+//     we still remove this redundant field (compatible with previous).
+func convertEmptySliceToEmptyStructPartly(objBytes []byte, isStruct bool, references ...any) ([]byte, error) {
+	// 1. if an empty lua object is originally struct type (not slice), just return {}; if slice, just return [].
+	if bytes.Equal(objBytes, []byte("[]")) {
+		if isStruct {
+			return []byte("{}"), nil
+		}
+		return []byte("[]"), nil
+	}
+
+	// 2. iterate through the json bytes of referenced objects and record the empty value fields.
+	fieldOfEmptySlice, fieldOfEmptyStruct := sets.New[string](), sets.New[string]()
+	for _, reference := range references {
+		jsonBytes, err := json.Marshal(reference)
+		if err != nil {
+			return objBytes, fmt.Errorf("json marshal reference failed: %+v", err)
+		}
+		jsonVal := gjson.Parse(string(jsonBytes))
+
+		fieldOfEmptySliceTemp, fieldOfEmptyStructTemp := traverseToFindEmptyField(jsonVal, nil)
+		fieldOfEmptySlice = fieldOfEmptySlice.Union(fieldOfEmptySliceTemp)
+		fieldOfEmptyStruct = fieldOfEmptyStruct.Union(fieldOfEmptyStructTemp)
+	}
+
+	// 3. iterate through the given json bytes, and check an empty slice field whether ever existed in referenced objects.
+	objJSONStr := string(objBytes)
+	objJSON := gjson.Parse(objJSONStr)
+	fieldOfEmptySliceToStruct, fieldOfEmptySliceToDelete := traverseToFindEmptyFieldNeededModify(objJSON, nil, nil, fieldOfEmptySlice, fieldOfEmptyStruct)
+
+	// 4. if an empty slice field is originally a struct, change it back to {}; if originally not exist, delete the field.
+	var err error
+	for _, fieldPath := range fieldOfEmptySliceToStruct.UnsortedList() {
+		objJSONStr, err = sjson.Set(objJSONStr, fieldPath, struct{}{})
+		if err != nil {
+			return objBytes, fmt.Errorf("sjson set empty slice to empty struct failed: %+v", err)
+		}
+	}
+	for _, fieldPath := range fieldOfEmptySliceToDelete.UnsortedList() {
+		objJSONStr, err = sjson.Delete(objJSONStr, fieldPath)
+		if err != nil {
+			return objBytes, fmt.Errorf("sjson delete empty slice field failed: %+v", err)
+		}
+	}
+
+	return []byte(objJSONStr), nil
+}
+
+// traverseToFindEmptyField get the field path with empty values by traverse a gjson.Result
+//
+// e.g: root={"spec":{"aa":{},"bb":[],"cc":["x"],"dd":{"ee":{}}}}
+// we traverse the root and record every level filed name into `fieldPath` variable.
+//  1. when traverse to the field `spec.aa`, we got an empty struct, so add it into `fieldOfEmptyStruct` variable.
+//  2. when traverse to the field `spec.bb`, we got an empty slice, so add it into `fieldOfEmptySlice` variable.
+//  3. when traverse to the field `spec.dd.ee`, we got another empty struct, so add it into `fieldOfEmptyStruct` variable.
+//
+// So, finally, fieldOfEmptySlice={"spec.bb"}, fieldOfEmptyStruct={"spec.aa", "spec.dd.ee"}
+func traverseToFindEmptyField(root gjson.Result, fieldPath []string) (sets.Set[string], sets.Set[string]) {
+	rootIsNotArray := !root.IsArray()
+	fieldOfEmptySlice, fieldOfEmptyStruct := sets.New[string](), sets.New[string]()
+
+	root.ForEach(func(key, value gjson.Result) bool {
+		curFieldPath := fieldPath
+		// e.g: root={"rules":[{"ruleName":[]}}]}, when we traverse `rules` field, which is an array.
+		// the first element is key=0, value={"ruleName":[]}
+		// however, we expected to record the `ruleName` field path as `rules.ruleName`, rather than `rules.0.ruleName`.
+		if rootIsNotArray {
+			curFieldPath = append(fieldPath, key.String())
+		}
+		curFieldStr := strings.Join(curFieldPath, ".")
+
+		if value.IsArray() && len(value.Array()) == 0 {
+			fieldOfEmptySlice.Insert(curFieldStr)
+		} else if value.IsObject() && len(value.Map()) == 0 {
+			fieldOfEmptyStruct.Insert(curFieldStr)
+		} else if value.IsArray() || value.IsObject() {
+			childEmptySlice, childEmptyStruct := traverseToFindEmptyField(value, curFieldPath)
+			fieldOfEmptySlice = fieldOfEmptySlice.Union(childEmptySlice)
+			fieldOfEmptyStruct = fieldOfEmptyStruct.Union(childEmptyStruct)
+		}
+
+		return true // keep iterating
+	})
+
+	return fieldOfEmptySlice, fieldOfEmptyStruct
+}
+
+// traverseToFindEmptyFieldNeededModify find the field with empty values which needed to be modified by traverse a gjson.Result
+//
+// e.g: root={"spec":{"aa":[],"bb":[],"cc":["x"],"dd":{"ee":[],"ff":[]}}}
+// fieldOfEmptySlice={"spec.bb"}, fieldOfEmptyStruct={"spec.aa", "spec.dd.ee"}
+//
+// we traverse the root and record every level filed name into `fieldPath`、`fieldPathWithArrayIndex` variable,
+// then judge whether the field path exist in `fieldOfEmptySlice` or `fieldOfEmptyStruct` variable:
+//  1. when traverse to the field `spec.aa`, we got an empty slice, but this filed exists in `fieldOfEmptyStruct`,
+//     so, it originally is struct, which needed to be modified back, we add it into `fieldOfEmptySliceToStruct` variable.
+//  2. when traverse to the field `spec.bb`, we got an empty slice, and it exists in `fieldOfEmptySlice`,
+//     so, it originally is slice, which should keep unchanged.
+//  3. when traverse to the field `spec.dd.ee`, we got an empty slice, but it also exists in `fieldOfEmptyStruct`,
+//     so, it originally is struct too, we add it into `fieldOfEmptySliceToStruct` variable.
+//  4. when traverse to the field `spec.dd.ff`, we got an empty slice, but it not exists in either map variable,
+//     so, it orinally not exist, we can't judge whether it is struct, so we add it into `fieldOfEmptySliceToDelete` variable to remove it.
+//
+// So, finally, fieldOfEmptySliceToStruct={"spec.aa", "spec.dd.ee"}, fieldOfEmptySliceToDelete={"spec.dd.ff"}
+func traverseToFindEmptyFieldNeededModify(root gjson.Result, fieldPath, fieldPathWithArrayIndex []string, fieldOfEmptySlice, fieldOfEmptyStruct sets.Set[string]) (sets.Set[string], sets.Set[string]) {
+	rootIsNotArray := !root.IsArray()
+	fieldOfEmptySliceToStruct, fieldOfEmptySliceToDelete := sets.New[string](), sets.New[string]()
+
+	root.ForEach(func(key, value gjson.Result) bool {
+		curFieldPath := fieldPath
+		// e.g: root={"rules":[{"ruleName":[]}}]}, when we traverse `rules` field, which is an array.
+		// the first element is key=0, value={"ruleName":[]}
+		// we record `rules.ruleName` into curFieldPath, and record `rules.0.ruleName` into curFieldPathWithArrayIndex.
+		if rootIsNotArray {
+			curFieldPath = append(fieldPath, key.String())
+		}
+		curFieldPathWithArrayIndex := append(fieldPathWithArrayIndex, key.String())
+
+		if value.IsArray() && len(value.Array()) == 0 {
+			curFieldPathStr := strings.Join(curFieldPath, ".")
+			curFieldPathWithIndexStr := strings.Join(curFieldPathWithArrayIndex, ".")
+
+			if fieldOfEmptyStruct.Has(curFieldPathStr) {
+				// if an empty object filed is originally an empty struct, we need to modify it back to struct.
+				fieldOfEmptySliceToStruct.Insert(curFieldPathWithIndexStr)
+			} else if !fieldOfEmptySlice.Has(curFieldPathStr) {
+				// if an empty object filed is originally not exist (neither slice nor struct), we need to delete it.
+				fieldOfEmptySliceToDelete.Insert(curFieldPathWithIndexStr)
+			}
+		} else if value.IsArray() || value.IsObject() {
+			childEmptySliceToStruct, childEmptySliceToDelete := traverseToFindEmptyFieldNeededModify(value, curFieldPath,
+				curFieldPathWithArrayIndex, fieldOfEmptySlice, fieldOfEmptyStruct)
+			fieldOfEmptySliceToStruct = fieldOfEmptySliceToStruct.Union(childEmptySliceToStruct)
+			fieldOfEmptySliceToDelete = fieldOfEmptySliceToDelete.Union(childEmptySliceToDelete)
+		}
+
+		return true // keep iterating
+	})
+
+	return fieldOfEmptySliceToStruct, fieldOfEmptySliceToDelete
 }
 
 // ConvertLuaResultToInt convert lua result to int.
--- pkg/resourceinterpreter/customized/declarative/luavm/lua_convert_test.go
+++ pkg/resourceinterpreter/customized/declarative/luavm/lua_convert_test.go
@@ -17,13 +17,19 @@ limitations under the License.
 package luavm
 
 import (
+	"encoding/json"
 	"reflect"
 	"testing"
 
+	"github.com/tidwall/gjson"
 	lua "github.com/yuin/gopher-lua"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/util/sets"
+
+	configv1alpha1 "github.com/karmada-io/karmada/pkg/apis/config/v1alpha1"
 )
 
-func TestConvertLuaResultInto(t *testing.T) {
+func TestConvertLuaResultToStruct(t *testing.T) {
 	type barStruct struct {
 		Bar string
 	}
@@ -31,8 +37,19 @@ func TestConvertLuaResultInto(t *testing.T) {
 		Bar barStruct
 	}
 
+	type specStruct struct {
+		EmptyMap      map[string]string
+		NonEmptyMap   map[string]string
+		EmptySlice    []string
+		NonEmptySlice []string
+		OtherField    string
+	}
+	type foolStruct struct {
+		Spec specStruct
+	}
+
 	type args struct {
-		luaResult lua.LValue
+		luaResult *lua.LTable
 		obj       interface{}
 	}
 	tests := []struct {
@@ -42,7 +59,7 @@ func TestConvertLuaResultInto(t *testing.T) {
 		want    interface{}
 	}{
 		{
-			name: "not a pointer",
+			name: "obj is not a pointer",
 			args: args{
 				luaResult: &lua.LTable{},
 				obj:       struct{}{},
@@ -60,32 +77,9 @@ func TestConvertLuaResultInto(t *testing.T) {
 			want:    &fooStruct{},
 		},
 		{
-			name: "empty table into slice",
+			name: "table with empty table into struct",
 			args: args{
-				luaResult: &lua.LTable{},
-				obj:       &[]string{},
-			},
-			wantErr: false,
-			want:    &[]string{},
-		},
-		{
-			name: "non-empty table into slice",
-			args: args{
-				luaResult: func() lua.LValue {
-					v := &lua.LTable{}
-					v.Append(lua.LString("foo"))
-					v.Append(lua.LString("bar"))
-					return v
-				}(),
-				obj: &[]string{},
-			},
-			wantErr: false,
-			want:    &[]string{"foo", "bar"},
-		},
-		{
-			name: "table with empty table into slice",
-			args: args{
-				luaResult: func() lua.LValue {
+				luaResult: func() *lua.LTable {
 					v := &lua.LTable{}
 					v.RawSetString("Bar", &lua.LTable{})
 					return v
@@ -98,7 +92,7 @@ func TestConvertLuaResultInto(t *testing.T) {
 		{
 			name: "struct is not empty, and convert successfully",
 			args: args{
-				luaResult: func() lua.LValue {
+				luaResult: func() *lua.LTable {
 					bar := &lua.LTable{}
 					bar.RawSetString("Bar", lua.LString("bar"))
 
@@ -113,14 +107,344 @@ func TestConvertLuaResultInto(t *testing.T) {
 				Bar: barStruct{Bar: "bar"},
 			},
 		},
+		{
+			name: "empty table into slice",
+			args: args{
+				luaResult: &lua.LTable{},
+				obj:       &[]string{},
+			},
+			wantErr: false,
+			want:    &[]string{},
+		},
+		{
+			name: "non-empty table into slice",
+			args: args{
+				luaResult: func() *lua.LTable {
+					v := &lua.LTable{}
+					v.Append(lua.LString("foo"))
+					v.Append(lua.LString("bar"))
+					return v
+				}(),
+				obj: &[]string{},
+			},
+			wantErr: false,
+			want:    &[]string{"foo", "bar"},
+		},
+		{
+			name: "table into DependentObjectReference slice",
+			args: args{
+				luaResult: func() *lua.LTable {
+					item := &lua.LTable{}
+					item.RawSetString("apiVersion", lua.LString("demo"))
+					item.RawSetString("kind", lua.LString("demo"))
+
+					v := &lua.LTable{}
+					v.Append(item)
+					return v
+				}(),
+				obj: &[]configv1alpha1.DependentObjectReference{},
+			},
+			wantErr: false,
+			want: &[]configv1alpha1.DependentObjectReference{{
+				APIVersion: "demo",
+				Kind:       "demo",
+			}},
+		},
+		{
+			name: "convert struct with empty field",
+			args: args{
+				luaResult: func() *lua.LTable {
+					innerMap := &lua.LTable{}
+					innerMap.RawSetString("test-key", lua.LString(`\"trap-string\":[]`))
+
+					innerSlice := &lua.LTable{}
+					innerSlice.Append(lua.LString(`\"trap-string\":[]`))
+
+					spec := &lua.LTable{}
+					spec.RawSetH(lua.LString("EmptyMap"), &lua.LTable{})
+					spec.RawSetH(lua.LString("NonEmptyMap"), innerMap)
+					spec.RawSetH(lua.LString("EmptySlice"), &lua.LTable{})
+					spec.RawSetH(lua.LString("NonEmptySlice"), innerSlice)
+					spec.RawSetString("OtherField", lua.LString(`\"trap-string\":[]`))
+
+					v := &lua.LTable{}
+					v.RawSetString("Spec", spec)
+					return v
+				}(),
+				obj: &foolStruct{},
+			},
+			wantErr: false,
+			want: &foolStruct{
+				Spec: specStruct{
+					NonEmptyMap:   map[string]string{"test-key": `\"trap-string\":[]`},
+					NonEmptySlice: []string{`\"trap-string\":[]`},
+					OtherField:    `\"trap-string\":[]`,
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := ConvertLuaResultInto(tt.args.luaResult, tt.args.obj); (err != nil) != tt.wantErr {
-				t.Errorf("ConvertLuaResultInto() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("ConvertLuaResultToStruct() error = %v, wantErr %v", err, tt.wantErr)
 			}
 			if got := tt.args.obj; !reflect.DeepEqual(tt.want, got) {
+				t.Errorf("ConvertLuaResultToStruct() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestConvertLuaResultToUnstruct(t *testing.T) {
+	type args struct {
+		luaResult  *lua.LTable
+		references []*unstructured.Unstructured
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *unstructured.Unstructured
+		wantErr bool
+	}{
+		{
+			name: "convert unstructured with empty field not referring to references",
+			args: args{
+				luaResult: func() *lua.LTable {
+					innerMap := &lua.LTable{}
+					innerMap.RawSetString("test-key", lua.LString(`\"trap-string\":[]`))
+
+					innerSlice := &lua.LTable{}
+					innerSlice.Append(lua.LString(`\"trap-string\":[]`))
+
+					spec := &lua.LTable{}
+					spec.RawSetH(lua.LString("EmptyMap"), &lua.LTable{})
+					spec.RawSetH(lua.LString("NonEmptyMap"), innerMap)
+					spec.RawSetH(lua.LString("EmptySlice"), &lua.LTable{})
+					spec.RawSetH(lua.LString("NonEmptySlice"), innerSlice)
+					spec.RawSetString("OtherField", lua.LString(`\"trap-string\":[]`))
+
+					v := &lua.LTable{}
+					v.RawSetString("kind", lua.LString("demo"))
+					v.RawSetString("spec", spec)
+					return v
+				}(),
+				references: nil,
+			},
+			wantErr: false,
+			want: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"kind": "demo",
+					"spec": map[string]interface{}{
+						"NonEmptyMap":   map[string]interface{}{"test-key": `\"trap-string\":[]`},
+						"NonEmptySlice": []interface{}{`\"trap-string\":[]`},
+						"OtherField":    `\"trap-string\":[]`,
+					},
+				},
+			},
+		},
+		{
+			name: "convert unstructured with empty field referring to references",
+			args: args{
+				luaResult: func() *lua.LTable {
+					innerMap := &lua.LTable{}
+					innerMap.RawSetString("test-key", lua.LString(`\"trap-string\":[]`))
+
+					innerSlice := &lua.LTable{}
+					innerSlice.Append(lua.LString(`\"trap-string\":[]`))
+
+					spec := &lua.LTable{}
+					spec.RawSetH(lua.LString("EmptyMap"), &lua.LTable{})
+					spec.RawSetH(lua.LString("NonEmptyMap"), innerMap)
+					spec.RawSetH(lua.LString("EmptySlice"), &lua.LTable{})
+					spec.RawSetH(lua.LString("NonEmptySlice"), innerSlice)
+					spec.RawSetString("OtherField", lua.LString(`\"trap-string\":[]`))
+
+					v := &lua.LTable{}
+					v.RawSetString("kind", lua.LString("demo"))
+					v.RawSetString("spec", spec)
+					return v
+				}(),
+				references: []*unstructured.Unstructured{{
+					Object: map[string]interface{}{
+						"kind": "demo",
+						"spec": map[string]interface{}{"EmptySlice": []interface{}{}, "EmptyMap": map[string]interface{}{}}},
+				}},
+			},
+			wantErr: false,
+			want: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"kind": "demo",
+					"spec": map[string]interface{}{
+						"EmptyMap":      map[string]interface{}{},
+						"NonEmptyMap":   map[string]interface{}{"test-key": `\"trap-string\":[]`},
+						"EmptySlice":    []interface{}{},
+						"NonEmptySlice": []interface{}{`\"trap-string\":[]`},
+						"OtherField":    `\"trap-string\":[]`,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got = &unstructured.Unstructured{}
+			err := ConvertLuaResultInto(tt.args.luaResult, got, tt.args.references)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ConvertLuaResultInto() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ConvertLuaResultInto() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_traverseToFindEmptyField(t *testing.T) {
+	type args struct {
+		root      gjson.Result
+		fieldPath []string
+	}
+	type wants struct {
+		fieldOfEmptySlice  sets.Set[string]
+		fieldOfEmptyStruct sets.Set[string]
+	}
+	tests := []struct {
+		name  string
+		args  args
+		wants wants
+	}{
+		{
+			name: "traverse to find empty field",
+			args: args{
+				root:      gjson.Parse(`{"spec":[{"aa":{},"bb":[],"cc":["x"],"dd":{"ee":{}}}]}`),
+				fieldPath: nil,
+			},
+			wants: wants{
+				fieldOfEmptySlice:  sets.New[string]("spec.bb"),
+				fieldOfEmptyStruct: sets.New[string]("spec.aa", "spec.dd.ee"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fieldOfEmptySlice, fieldOfEmptyStruct := traverseToFindEmptyField(tt.args.root, tt.args.fieldPath)
+
+			if got := fieldOfEmptySlice; !reflect.DeepEqual(tt.wants.fieldOfEmptySlice, got) {
+				t.Errorf("traverseToFindEmptyField() got fieldOfEmptySlice = %v, want %v",
+					got, tt.wants.fieldOfEmptySlice)
+			}
+
+			if got := fieldOfEmptyStruct; !reflect.DeepEqual(tt.wants.fieldOfEmptyStruct, got) {
+				t.Errorf("traverseToFindEmptyField() got fieldOfEmptyStruct = %v, want %v",
+					got, tt.wants.fieldOfEmptyStruct)
+			}
+		})
+	}
+}
+
+func Test_traverseToFindEmptyFieldNeededModify(t *testing.T) {
+	type args struct {
+		root                    gjson.Result
+		fieldPath               []string
+		fieldPathWithArrayIndex []string
+		fieldOfEmptySlice       sets.Set[string]
+		fieldOfEmptyStruct      sets.Set[string]
+	}
+	type wants struct {
+		fieldOfEmptySliceToStruct sets.Set[string]
+		fieldOfEmptySliceToDelete sets.Set[string]
+	}
+	tests := []struct {
+		name  string
+		args  args
+		wants wants
+	}{
+		{
+			name: "traverse to find empty field needed modify",
+			args: args{
+				root:                    gjson.Parse(`{"spec":[{"aa":[],"bb":[],"cc":["x"],"dd":{"ee":[],"ff":[]}}]}`),
+				fieldPath:               nil,
+				fieldPathWithArrayIndex: nil,
+				fieldOfEmptySlice:       sets.New[string]("spec.bb"),
+				fieldOfEmptyStruct:      sets.New[string]("spec.aa", "spec.dd.ee"),
+			},
+			wants: wants{
+				fieldOfEmptySliceToStruct: sets.New[string]("spec.0.aa", "spec.0.dd.ee"),
+				fieldOfEmptySliceToDelete: sets.New[string]("spec.0.dd.ff"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fieldOfEmptySliceToStruct, fieldOfEmptySliceToDelete := traverseToFindEmptyFieldNeededModify(tt.args.root,
+				tt.args.fieldPath, tt.args.fieldPathWithArrayIndex, tt.args.fieldOfEmptySlice, tt.args.fieldOfEmptyStruct)
+
+			if got := fieldOfEmptySliceToStruct; !reflect.DeepEqual(tt.wants.fieldOfEmptySliceToStruct, got) {
+				t.Errorf("traverseToFindEmptyFieldNeededModify() got fieldOfEmptySliceToStruct = %v, want %v",
+					got, tt.wants.fieldOfEmptySliceToStruct)
+			}
+
+			if got := fieldOfEmptySliceToDelete; !reflect.DeepEqual(tt.wants.fieldOfEmptySliceToDelete, got) {
+				t.Errorf("traverseToFindEmptyFieldNeededModify() got fieldOfEmptySliceToDelete = %v, want %v",
+					got, tt.wants.fieldOfEmptySliceToDelete)
+			}
+		})
+	}
+}
+
+func Test_convertEmptyObjectBackToEmptySlice(t *testing.T) {
+	type args struct {
+		objBytes   []byte
+		isStruct   bool
+		references []*unstructured.Unstructured
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+		want    []byte
+	}{
+		{
+			name: "convert empty object back to empty slice",
+			args: args{
+				objBytes: []byte(`{"spec":[{"aa":[],"bb":[],"cc":["x"],"dd":{"ee":[],"ff":[]}}]}`),
+				isStruct: true,
+				references: func() []*unstructured.Unstructured {
+					desired, observed := &unstructured.Unstructured{}, &unstructured.Unstructured{}
+					_ = json.Unmarshal([]byte(`{"kind": "demo", "spec":[{"aa":{},"bb":[]}]}`), desired)
+					_ = json.Unmarshal([]byte(`{"kind": "demo", "spec":[{"dd":{"ee":{}}}]}`), observed)
+					return []*unstructured.Unstructured{desired, observed}
+				}(),
+			},
+			wantErr: false,
+			want:    []byte(`{"spec":[{"aa":{},"bb":[],"cc":["x"],"dd":{"ee":{}}}]}`),
+		},
+		{
+			name: "obj is not struct while objBytes is []",
+			args: args{
+				objBytes: []byte(`[]`),
+				isStruct: false,
+				references: func() []*unstructured.Unstructured {
+					desired, observed := &unstructured.Unstructured{}, &unstructured.Unstructured{}
+					_ = json.Unmarshal([]byte(`{"kind": "demo", "spec":[{"aa":{},"bb":[]}]}`), desired)
+					_ = json.Unmarshal([]byte(`{"kind": "demo", "spec":[{"dd":{"ee":{}}}]}`), observed)
+					return []*unstructured.Unstructured{desired, observed}
+				}(),
+			},
+			wantErr: false,
+			want:    []byte(`[]`),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := convertEmptySliceToEmptyStructPartly(tt.args.objBytes, tt.args.isStruct, tt.args.references)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("convertEmptySliceToEmptyStructPartly() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("convertEmptySliceToEmptyStructPartly() got = %s, want %s", got, tt.want)
 			}
 		})
 	}
