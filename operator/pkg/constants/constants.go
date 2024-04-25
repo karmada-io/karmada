71e203ea335fb9f07ae7798329fd44e95a3d0144
Rohit Satya <work.rohitsatya@gmail.com>
2024-04-25 09:52:24 +0530

    etcd:Update version to 3.5.13
    Signed-off-by: Rohit Satya <work.rohitsatya@gmail.com>

--- operator/pkg/constants/constants.go
+++ operator/pkg/constants/constants.go
@@ -29,7 +29,7 @@ const (
 	// KarmadaDefaultRepository defines the default of the karmada image repository
 	KarmadaDefaultRepository = "docker.io/karmada"
 	// EtcdDefaultVersion defines the default of the karmada etcd image tag
-	EtcdDefaultVersion = "3.5.9-0"
+	EtcdDefaultVersion = "3.5.13-0"
 	// KubeDefaultVersion defines the default of the karmada apiserver and kubeControllerManager image tag
 	KubeDefaultVersion = "v1.27.11"
 	// KarmadaDefaultServiceSubnet defines the default of the subnet used by k8s services.
