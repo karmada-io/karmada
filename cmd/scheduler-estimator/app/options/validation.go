540a116f47bb19567ea5b56d8aa9fbb5eb3faaa3
wei-chenglai <qazwsx0939059006@gmail.com>
2024-07-28 14:55:27 -0400

    Standardize the health probe & metrics arguments of scheduler-estimator
    Signed-off-by: wei-chenglai <qazwsx0939059006@gmail.com>

--- cmd/scheduler-estimator/app/options/validation.go
+++ cmd/scheduler-estimator/app/options/validation.go
@@ -17,8 +17,6 @@ limitations under the License.
 package options
 
 import (
-	"net"
-
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
@@ -31,16 +29,8 @@ func (o *Options) Validate() field.ErrorList {
 		errs = append(errs, field.Invalid(newPath.Child("ClusterName"), o.ClusterName, "clusterName cannot be empty"))
 	}
 
-	if net.ParseIP(o.BindAddress) == nil {
-		errs = append(errs, field.Invalid(newPath.Child("BindAddress"), o.BindAddress, "not a valid textual representation of an IP address"))
-	}
-
 	if o.ServerPort < 0 || o.ServerPort > 65535 {
 		errs = append(errs, field.Invalid(newPath.Child("ServerPort"), o.ServerPort, "must be a valid port between 0 and 65535 inclusive"))
-	}
-
-	if o.SecurePort < 0 || o.SecurePort > 65535 {
-		errs = append(errs, field.Invalid(newPath.Child("SecurePort"), o.SecurePort, "must be a valid port between 0 and 65535 inclusive"))
 	}
 
 	return errs
