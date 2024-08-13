82066b69e3f6a68fe3fa7c8d7d9accb7920da4fe
hexiaofeng <hexiaofeng@jd.com>
2024-08-14 07:43:46 +0800

    Go imports

--- pkg/scheduler/core/spreadconstraint/select_clusters_test.go
+++ pkg/scheduler/core/spreadconstraint/select_clusters_test.go
@@ -18,13 +18,14 @@ package spreadconstraint
 
 import (
 	"fmt"
+	"reflect"
+	"testing"
+
 	clusterv1alpha1 "github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1"
 	policyv1alpha1 "github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1"
 	workv1alpha2 "github.com/karmada-io/karmada/pkg/apis/work/v1alpha2"
 	"github.com/karmada-io/karmada/pkg/scheduler/framework"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"reflect"
-	"testing"
 )
 
 // newCluster will build a Cluster with topology.
