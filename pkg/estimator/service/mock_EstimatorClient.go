93c86ec49e73ddee172dde61eaf9657294b47200
lzhou286 <lzhou286@bloomberg.net>
2024-08-26 12:29:46 -0400

    Add preemption grpc api and endpoint to estimator service
    Signed-off-by: lzhou286 <lzhou286@bloomberg.net>

--- pkg/estimator/service/mock_EstimatorClient.go
+++ pkg/estimator/service/mock_EstimatorClient.go
@@ -63,6 +63,36 @@ func (_m *MockEstimatorClient) GetUnschedulableReplicas(ctx context.Context, in 
 	return r0, r1
 }
 
+// GetVictimResourceBindings provides a mock function with given fields: ctx, in, opts
+func (_m *MockEstimatorClient) GetVictimResourceBindings(ctx context.Context, in *pb.PreemptionRequest, opts ...grpc.CallOption) (*pb.PreemptionResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *pb.PreemptionResponse
+	if rf, ok := ret.Get(0).(func(context.Context, *pb.PreemptionRequest, ...grpc.CallOption) *pb.PreemptionResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*pb.PreemptionResponse)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *pb.PreemptionRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // MaxAvailableReplicas provides a mock function with given fields: ctx, in, opts
 func (_m *MockEstimatorClient) MaxAvailableReplicas(ctx context.Context, in *pb.MaxAvailableReplicasRequest, opts ...grpc.CallOption) (*pb.MaxAvailableReplicasResponse, error) {
 	_va := make([]interface{}, len(opts))
