87d83fce2421c02c9a34bb3a2f08905aa035dde5
zhzhuang-zju <m17799853869@163.com>
2024-11-27 16:47:58 +0800

    karmadactl init: add CRDs archive verification to enhance file system robustness
    Signed-off-by: zhzhuang-zju <m17799853869@163.com>
    Signed-off-by: Akash <akashsingh2210670@gmail.com>

--- pkg/karmadactl/cmdinit/utils/util.go
+++ pkg/karmadactl/cmdinit/utils/util.go
@@ -157,3 +157,17 @@ func ListFiles(path string) []string {
 	}
 	return files
 }
+
+// PathExists check whether the path is exist
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	return false, err
+}
