b9a6fa1157711b6662684c373ba13197a1ed5851
zhzhuang-zju <m17799853869@163.com>
2025-04-08 11:11:21 +0800

    fix the issue where discovery-timeout fails to work properly
    Signed-off-by: zhzhuang-zju <m17799853869@163.com>

--- pkg/karmadactl/register/register.go
+++ pkg/karmadactl/register/register.go
@@ -444,7 +444,7 @@ func (o *CommandRegisterOption) EnsureNecessaryResourcesExistInMemberCluster(boo
 		return err
 	}
 
-	if err = cmdutil.WaitForDeploymentRollout(o.memberClusterClient, KarmadaAgentDeployment, int(o.Timeout)); err != nil {
+	if err = cmdutil.WaitForDeploymentRollout(o.memberClusterClient, KarmadaAgentDeployment, o.Timeout); err != nil {
 		return err
 	}
 
