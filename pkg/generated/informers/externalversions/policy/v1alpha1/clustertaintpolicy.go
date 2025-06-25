95f9c54778368ee6c8de07d81c13c352ccb478ab
RainbowMango <qdurenhongcai@gmail.com>
2025-06-25 16:28:01 +0800

    Update codegen
    Signed-off-by: RainbowMango <qdurenhongcai@gmail.com>

--- pkg/generated/informers/externalversions/policy/v1alpha1/clustertaintpolicy.go
+++ pkg/generated/informers/externalversions/policy/v1alpha1/clustertaintpolicy.go
@@ -61,13 +61,25 @@ func NewFilteredClusterTaintPolicyInformer(client versioned.Interface, resyncPer
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.PolicyV1alpha1().ClusterTaintPolicies().List(context.TODO(), options)
+				return client.PolicyV1alpha1().ClusterTaintPolicies().List(context.Background(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.PolicyV1alpha1().ClusterTaintPolicies().Watch(context.TODO(), options)
+				return client.PolicyV1alpha1().ClusterTaintPolicies().Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.PolicyV1alpha1().ClusterTaintPolicies().List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.PolicyV1alpha1().ClusterTaintPolicies().Watch(ctx, options)
 			},
 		},
 		&apispolicyv1alpha1.ClusterTaintPolicy{},
