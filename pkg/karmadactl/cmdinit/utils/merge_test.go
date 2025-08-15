7a163942309db9eac0066d6170921cc9a45756fd
luyb177 <2085661244@qq.com>
2025-08-15 16:33:34 +0800

    feat:Add test files. In the Example, add detailed explanations for the use of extraArgs in each component. Modify the functions in merge.go to resolve issues such as leading and trailing spaces in the command line.
    Signed-off-by: luyb177 <2085661244@qq.com>
    
    feat:Add a performance testing function that adds capacity to each slice to avoid multiple memory allocations.
    
    Signed-off-by: luyb177 <2085661244@qq.com>

--- pkg/karmadactl/cmdinit/utils/merge_test.go
+++ pkg/karmadactl/cmdinit/utils/merge_test.go
@@ -368,6 +368,7 @@ var errCase = []componentCommandCase{
 			"--authorization-mode=Node,RBAC",
 			"--key=value",
 		},
+
 		err: nil,
 	},
 
@@ -403,6 +404,7 @@ var errCase = []componentCommandCase{
 // Abstract a function that runs multiple test cases.
 func testCase(cases []componentCommandCase, t *testing.T) {
 	for _, c := range cases {
+
 		got, err := KarmadaComponentCommand(defaultArgs, c.extraArgs)
 		if err != nil {
 			if c.err == nil {
