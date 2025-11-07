ca978831fe0856777f823af6c37465891d304e29
zach593 <zach_li@outlook.com>
2025-11-07 17:12:51 +0800

    feat: introduce asyncPriorityWorker in resourceDetector Processor
    Signed-off-by: zach593 <zach_li@outlook.com>

--- pkg/util/fedinformer/handlers.go
+++ pkg/util/fedinformer/handlers.go
@@ -63,13 +63,13 @@ func NewHandlerOnEvents(addFunc func(obj interface{}), updateFunc func(oldObj, n
 // coming in, ensuring the appropriate nested handler method is invoked.
 //
 // Note: An object that starts passing the filter after an update is considered an add, and
-// an object that stops passing the filter after an update is considered a delete.
+// an object that stops passing the filter after an update is considered a deletion.
 // Like the handlers, the filter MUST NOT modify the objects it is given.
-func NewFilteringHandlerOnAllEvents(filterFunc func(obj interface{}) bool, addFunc func(obj interface{}),
+func NewFilteringHandlerOnAllEvents(filterFunc func(obj interface{}) bool, addFunc func(obj interface{}, isInitialList bool),
 	updateFunc func(oldObj, newObj interface{}), deleteFunc func(obj interface{})) cache.ResourceEventHandler {
 	return &cache.FilteringResourceEventHandler{
 		FilterFunc: filterFunc,
-		Handler: cache.ResourceEventHandlerFuncs{
+		Handler: cache.ResourceEventHandlerDetailedFuncs{
 			AddFunc:    addFunc,
 			UpdateFunc: updateFunc,
 			DeleteFunc: deleteFunc,
