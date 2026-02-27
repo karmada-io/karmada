cd6670ac5c2dec76f091aa1c3a80362b278e4e96
changzhen <changzhen5@huawei.com>
2026-02-27 11:56:46 +0800

    update client ratelimiter in e2e
    Signed-off-by: changzhen <changzhen5@huawei.com>

--- test/e2e/framework/cluster.go
+++ test/e2e/framework/cluster.go
@@ -92,8 +92,12 @@ func InitClusterInformation(karmadaClient karmada.Interface, controlPlaneClient 
 	gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 	gomega.Expect(meetRequirement).Should(gomega.BeTrue())
 
+	limiterGetter := util.GetClusterRateLimiterGetter()
+	clusterClientOption := &util.ClientOption{
+		RateLimiterGetter: limiterGetter.GetRateLimiter,
+	}
 	for _, cluster := range clusters {
-		clusterClient, clusterDynamicClient, err := newClusterClientSet(controlPlaneClient, cluster)
+		clusterClient, clusterDynamicClient, err := newClusterClientSet(controlPlaneClient, cluster, clusterClientOption)
 		gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 		clusterNames = append(clusterNames, cluster.Name)
 		clusterClients = append(clusterClients, clusterClient)
@@ -195,13 +199,14 @@ func isClusterMeetRequirements(clusters []*clusterv1alpha1.Cluster) (bool, error
 	return true, nil
 }
 
-func newClusterClientSet(controlPlaneClient client.Client, c *clusterv1alpha1.Cluster) (*util.ClusterClient, *util.DynamicClusterClient, error) {
+// newClusterClientSet will create cluster client and dynamic client according to the cluster's sync mode.
+func newClusterClientSet(controlPlaneClient client.Client, c *clusterv1alpha1.Cluster, clientOption *util.ClientOption) (*util.ClusterClient, *util.DynamicClusterClient, error) {
 	if c.Spec.SyncMode == clusterv1alpha1.Push {
-		clusterClient, err := util.NewClusterClientSet(c.Name, controlPlaneClient, nil)
+		clusterClient, err := util.NewClusterClientSet(c.Name, controlPlaneClient, clientOption)
 		if err != nil {
 			return nil, nil, err
 		}
-		clusterDynamicClient, err := util.NewClusterDynamicClientSet(c.Name, controlPlaneClient, nil)
+		clusterDynamicClient, err := util.NewClusterDynamicClientSet(c.Name, controlPlaneClient, clientOption)
 		if err != nil {
 			return nil, nil, err
 		}
@@ -212,6 +217,10 @@ func newClusterClientSet(controlPlaneClient client.Client, c *clusterv1alpha1.Cl
 	clusterConfig, err := LoadRESTClientConfig(clusterConfigPath, c.Name)
 	if err != nil {
 		return nil, nil, err
+	}
+
+	if clientOption != nil && clientOption.RateLimiterGetter != nil {
+		clusterConfig.RateLimiter = clientOption.RateLimiterGetter(c.Name)
 	}
 
 	clusterClientSet := util.ClusterClient{ClusterName: c.Name}
