6d41b4bf9a43968ab42ba7dac55048c32206110a
zach593 <zach_li@outlook.com>
2026-06-03 14:49:14 +0800

    feat(execution): only modify member cluster objects in execution controller
    Signed-off-by: zach593 <zach_li@outlook.com>

--- pkg/util/helper/cache.go
+++ pkg/util/helper/cache.go
@@ -27,12 +27,74 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	clusterv1alpha1 "github.com/karmada-io/karmada/pkg/apis/cluster/v1alpha1"
+	"github.com/karmada-io/karmada/pkg/util"
 	"github.com/karmada-io/karmada/pkg/util/fedinformer/genericmanager"
 	"github.com/karmada-io/karmada/pkg/util/fedinformer/keys"
 	"github.com/karmada-io/karmada/pkg/util/restmapper"
 )
+
+// EnsureInformerHandlersReady ensures the member cluster informers for the given resources exist
+// and have the handler registered. It returns whether those informers have already synced without waiting.
+func EnsureInformerHandlersReady(
+	cluster *clusterv1alpha1.Cluster,
+	gvrTargets []schema.GroupVersionResource,
+	handler cache.ResourceEventHandler,
+	manager genericmanager.MultiClusterInformerManager,
+	clusterClientSetFunc util.NewClusterDynamicClientSetFunc,
+	kubeClientSet client.Client,
+	clusterClientOption *util.ClientOption,
+) (bool, error) {
+	singleClusterInformerManager, err := getSingleClusterManager(cluster, manager, clusterClientSetFunc, kubeClientSet, clusterClientOption)
+	if err != nil {
+		return false, err
+	}
+
+	// We need an immediate check here and don't need to wait, the timeout is set to 0.
+	singleClusterInformerManager.WaitForCacheSyncWithTimeout(0)
+	allSynced := true
+	for _, gvr := range gvrTargets {
+		if !singleClusterInformerManager.IsHandlerExist(gvr, handler) {
+			allSynced = false
+			singleClusterInformerManager.ForResource(gvr, handler)
+			continue
+		}
+
+		if !singleClusterInformerManager.IsInformerSynced(gvr) {
+			allSynced = false
+		}
+	}
+	if allSynced {
+		return true, nil
+	}
+
+	manager.Start(cluster.Name)
+	return false, nil
+}
+
+func getSingleClusterManager(
+	cluster *clusterv1alpha1.Cluster,
+	manager genericmanager.MultiClusterInformerManager,
+	clusterClientSetFunc util.NewClusterDynamicClientSetFunc,
+	kubeClientSet client.Client,
+	clusterClientOption *util.ClientOption,
+) (genericmanager.SingleClusterInformerManager, error) {
+	singleClusterInformerManager := manager.GetSingleClusterManager(cluster.Name)
+	if singleClusterInformerManager != nil {
+		return singleClusterInformerManager, nil
+	}
+
+	dynamicClusterClient, err := clusterClientSetFunc(cluster.Name, kubeClientSet, clusterClientOption)
+	if err != nil {
+		klog.ErrorS(err, "Failed to build dynamic cluster client.", "cluster", cluster.Name)
+		return nil, err
+	}
+	return manager.ForCluster(dynamicClusterClient.ClusterName, dynamicClusterClient.DynamicClientSet, 0), nil
+}
 
 // GetObjectFromCache gets full object information from cache by key in worker queue.
 func GetObjectFromCache(
